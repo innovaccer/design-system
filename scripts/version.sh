9aa237b9c665df13c037512af569b36dbe0af6bd
Satyam Yadav <satyamyadav3@gmail.com>
2020-04-07 18:09:48 +0530

    chore: adds release workflow and action to release tag triggerd by release commits
    For triggering release commit message should be valid semver string e.g. v1.0.0-0
    Same version will be released as in commit message

---
+++ scripts/version.sh
@@ -0,0 +1,14 @@
+LOG=$(git log $(git describe --tags --abbrev=0 @^)..@ --pretty=format:"%h %s" --no-merges | sed -e 's/^/* /')
+PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+DATE=$(date +%F)
+
+# Append changelog
+echo "\n-------------------\n **v$PACKAGE_VERSION**  ($DATE) \n\n$LOG" >> CHANGELOG.md &&
+
+# build
+npm run build && 
+
+# add changes to git
+git add CHANGELOG.md &&
+git add dist -f && 
+git add css/dist -f
