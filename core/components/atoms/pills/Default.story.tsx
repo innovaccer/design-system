2a4e872d378b7bb2e7177a0a04bf1df6e4b6530e
Stuti Pandey <stutipandey1090@gmail.com>
2020-05-22 20:59:52 +0530

    feat: adds common stories

---
+++ core/components/atoms/pills/Default.story.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { text } from '@storybook/addon-knobs';
+import Pills, { Appearance } from '../pills';
+import Text from '@/components/atoms/text';
+
+// CSF format story
+export const defaultpill = () => {
+  const subtle = false;
+  const weight = 'strong';
+
+  const children = text('children', 'Pills');
+
+  const appearances: Appearance[] = ['secondary'];
+
+  const style = {
+    display: 'flex',
+  };
+
+  const innerStyle = {
+    marginRight: '5%',
+  };
+
+  return (
+    <div style={style}>
+      {
+        appearances.map((appear, ind) => {
+          return (
+            <div key={ind} style={innerStyle}>
+              <Pills
+                appearance={appear}
+                subtle={subtle}
+              >
+                {children}
+              </Pills>
+              <br />
+              <Text weight={weight}>{appear.charAt(0).toUpperCase() + appear.slice(1)}</Text>
+            </div>
+          );
+        })
+      }
+    </div>
+  );
+};
+
+export default {
+  title: 'Atoms|Pills',
+  component: Pills,
+  parameters: {
+    docs: {
+      docPage: {
+        title: 'Pills'
+      }
+    }
+  }
+};
