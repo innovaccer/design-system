9ba4ddf7c2450c2abd7437f8512aaf3db2d8c0ba
Stuti Pandey <stutipandey1090@gmail.com>
2020-06-18 22:49:30 +0530

    feat: adds successStatusHints

---
+++ core/components/atoms/statusHints/__stories__/success.story.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import StatusHints, { Appearance } from '../StatusHints';
+export const success = () => {
+  const appearances: Appearance[] = [
+    'success'
+  ];
+
+  return (
+    <div>
+      {
+        appearances.map((iconAppearance, i) => {
+          return (
+            <div key={i} style={{ marginBottom: '10px' }}>
+              <StatusHints
+                appearance={iconAppearance}
+              >
+                {iconAppearance.charAt(0).toUpperCase() + iconAppearance.slice(1)}
+              </StatusHints>
+            </div>
+          );
+        })
+      }
+    </div>
+  );
+};
+
+export default {
+  title: 'Atoms|StatusHints',
+  component: StatusHints,
+  parameters: {
+    docs: {
+      docPage: {
+        title: 'StatusHints'
+      }
+    }
+  }
+};
