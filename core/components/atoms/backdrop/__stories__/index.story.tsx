eaa7cf1539cb6d192e178c0fa89b04400339d998
Sandesh Choudhary <sandeshchoudhary5@gmail.com>
2020-07-07 08:20:24 +0530

    chore: modal/backdrop/dialog story updated & modal onClose replaced with backdropClose

--- core/components/atoms/backdrop/__stories__/index.story.tsx
+++ core/components/atoms/backdrop/__stories__/index.story.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { boolean } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
 import Backdrop from '../Backdrop';
-import { Heading } from '@/index';
+import { Navigation, Button, Text, PageHeader } from '@/index';
 
 export const all = () => {
   const open = boolean('open', true);
@@ -10,9 +11,37 @@ export const all = () => {
     open
   };
 
+  const navigationData = [
+    {
+      name: 'menu_1',
+      label: 'Menu 1',
+      icon: 'event',
+    },
+    {
+      name: 'menu_2',
+      label: 'Menu 2'
+    },
+    {
+      name: 'menu_3',
+      label: 'Menu 3',
+      disabled: true
+    }
+  ];
+
+  const pageHeaderOptions = {
+    title: 'Page Title',
+    navigation: <Navigation menus={navigationData} onClick={action('menu-clicked')} active={{ name: 'menu_1' }} />,
+    actions: (
+      <div style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
+        <span className="mr-4"><Text appearance="subtle">Meta data</Text></span>
+        <Button appearance="primary" onClick={action('Page header primary button click')}>Primary</Button>
+      </div>
+    )
+  };
+
   return (
-    <div style={{}}>
-      <Heading>Page background</Heading>
+    <div>
+      <PageHeader {...pageHeaderOptions} />
       <Backdrop {...options} />
     </div>
   );
