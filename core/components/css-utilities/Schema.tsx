903abb2f5d0dd41ff46c42ccf586f36905619aae
Stuti Pandey <stuti.pandey@innovaccer.com>
2020-07-15 14:41:08 +0530

    chore: adds CSS Utilities stories

---
+++ core/components/css-utilities/Schema.tsx
@@ -0,0 +1,20 @@
+import { Schema } from '../organisms/grid';
+
+const utilitiesSchema: Schema = [
+  {
+    name: 'className',
+    displayName: 'ClassName',
+    width: 500,
+    resizable: true,
+    align: 'left',
+  },
+  {
+    name: 'properties',
+    displayName: 'Properties',
+    width: 500,
+    resizable: true,
+    align: 'left',
+  },
+];
+
+export default utilitiesSchema;
---
+++ core/components/css-utilities/Spacing/Schema.tsx
@@ -0,0 +1,18 @@
+import { Schema } from '../../organisms/grid';
+const utilitiesSchema: Schema = [
+  {
+    name: 'marginClasses',
+    displayName: 'Margin Classes',
+    width: 500,
+    resizable: true,
+    align: 'left',
+  },
+  {
+    name: 'paddingClasses',
+    displayName: 'Padding Classes',
+    width: 500,
+    resizable: true,
+    align: 'left',
+  },
+];
+export default utilitiesSchema;
