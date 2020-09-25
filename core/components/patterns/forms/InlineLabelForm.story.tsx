17554ba5f64feddc0cfa646a3d3885e21694c194
Riya Lohia <riyalh1997@gmail.com>
2020-09-25 13:43:21 +0530

    feat(Forms-Patterns): adds advanced forms

---
+++ core/components/patterns/forms/InlineLabelForm.story.tsx
@@ -0,0 +1,210 @@
+import * as React from 'react';
+
+export const InlineLabelForm = () => <></>;
+
+const customCode = `
+// import * as React from 'react';
+// import { Input, Button, Label, Card, Dropdown, Text, Checkbox, Icon, Radio, InputMask } from 'design-system';
+
+() => {
+  const languages = [
+    {
+      label: 'Hindi',
+      value: 'Hindi',
+    },
+    {
+      label: 'English',
+      value: 'English',
+      selected: true
+    },
+    {
+      label: 'French',
+      value: 'French',
+    }
+  ];
+
+  class InlineLabelForm extends React.Component {
+    constructor(props = {}) {
+      super(props);
+
+      this.state = {
+        data: {
+          language: 'English',
+          defaultLanguage: 'English',
+          defaultPhoneNumber: 'primaryPhoneNumber'
+        },
+      };
+
+      this.onChange = this.onChange.bind(this);
+      this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(value, name) {
+      const { defaultLanguage = '' } = this.state.data;
+
+      const updatedData = {
+        ...this.state.data,
+        [name]: value,
+      };
+
+      this.setState({
+        data: updatedData,
+      });
+    }
+
+    onSubmit(event) {
+      event.preventDefault();
+      console.log(this.state.data);
+      return false;
+    }
+
+    render() {
+      const { defaultLanguage, language, defaultPhoneNumber } = this.state.data;
+
+      return (
+      <div className="w-100">
+        <Card className="px-6 py-6">
+          <form onSubmit={this.onSubmit}>
+            <Row className="mb-6">
+              <Column size={3} className="d-flex align-items-center">
+                <Icon className="mr-4" name="language" />
+                <Text>Known Languages</Text>
+              </Column>
+              <Column size={8} className="d-flex">
+                <div className="mr-5 w-25">
+                  <Dropdown
+                    options={languages}
+                    onChange={(value) => {
+                      const updatedData = {
+                        ...this.state.data,
+                        language: value,
+                        defaultLanguage: defaultLanguage !== '' ? value : defaultLanguage,
+                      };
+                      this.setState({data: updatedData})
+                    }}
+                  />
+                </div>
+                <Checkbox
+                  name="defaultLanguage"
+                  label="Set as Default"
+                  defaultChecked={defaultLanguage}
+                  onChange={(e) => {
+                    const updatedData = {
+                      ...this.state.data,
+                      defaultLanguage: e.target.checked ? language : '',
+                    };
+                    this.setState({data: updatedData})
+                  }}
+                />
+              </Column>
+              <Column size={1} className="d-flex align-items-center justify-content-end">
+                <Icon name="delete"/>
+              </Column>
+            </Row>
+            <Row className="my-5">
+              <Column className="d-flex align-items-center" size={3}>
+                <Icon className="mr-4" name="record_voice_over" />
+                <Text>Preferred Method of Contact</Text>
+              </Column>
+              <Column size={8} className="d-flex">
+                <Button type="button" className="mr-3" icon="call" onClick={() => this.onChange('phone', 'contact')}>Phone</Button>
+                <Button type="button" className="mr-3" icon="chat" onClick={() => this.onChange('message', 'contact')}>Message</Button>
+                <Button type="button" className="mr-3" icon="email" onClick={() => this.onChange('email', 'contact')}>Email</Button>
+                <Button
+                  type="button"
+                  className="mr-3"
+                  icon="markunread_mailbox"
+                  onClick={() => this.onChange('letter', 'contact')}
+                >
+                  Letter
+                </Button>
+              </Column>
+              <Column size={1} className="d-flex align-items-center justify-content-end">
+                <Icon name="delete"/>
+              </Column>
+            </Row>
+            <Row className="mt-6">
+              <Column size={3} className="d-flex align-items-center">
+                <Icon className="mr-4" name="call" />
+                <Text>Phone Numbers</Text>
+              </Column>
+              <Column size={8} className="d-flex">
+                <InputMask
+                  mask={['(', /[1-9]/, /\\d/, /\\d/, ')', ' ', /\\d/, /\\d/, /\\d/, '-', /\\d/, /\\d/, /\\d/, /\\d/]}
+                  placeholder="(___) ___-____"
+                  className="mr-4"
+                  name="primaryPhoneNumber"
+                  onChange={(e, value) => this.onChange(value, e.target.name)}
+                />
+                <Radio
+                  defaultChecked={defaultPhoneNumber === 'primaryPhoneNumber'}
+                  name="defaultPhoneNumber"
+                  value="defaultPrimaryPhoneNumber"
+                  label="Mark as Preferred"
+                  onChange={(e) => {
+                    const updatedData = {
+                      ...this.state.data,
+                      defaultPhoneNumber: e.target.checked ? 'primaryPhoneNumber' : defaultPhoneNumber
+                    };
+                    this.setState({data: updatedData})
+                  }}
+                />
+              </Column>
+              <Column size={1} className="d-flex align-items-center justify-content-end">
+                <Icon name="delete"/>
+              </Column>
+            </Row>
+            <Row className="my-5">
+              <Column size={3} className="d-flex align-items-center" />
+              <Column size={8} className="d-flex">
+                <InputMask
+                  mask={['(', /[1-9]/, /\\d/, /\\d/, ')', ' ', /\\d/, /\\d/, /\\d/, '-', /\\d/, /\\d/, /\\d/, /\\d/]}
+                  placeholder="(___) ___-____"
+                  className="mr-4"
+                  name="secondaryPhoneNumber"
+                  onChange={(e, value) => this.onChange(value, e.target.name)}
+                />
+                <Radio
+                  defaultChecked={defaultPhoneNumber === 'secondaryPhoneNumber'}
+                  name="defaultPhoneNumber"
+                  value="defaultSecondaryPhoneNumber"
+                  label="Mark as Preferred"
+                  onChange={(e) => {
+                    const updatedData = {
+                      ...this.state.data,
+                      defaultPhoneNumber: e.target.checked ? 'secondaryPhoneNumber' : defaultPhoneNumber
+                    };
+                    this.setState({data: updatedData})
+                  }}
+                />
+              </Column>
+              <Column size={1} className="d-flex align-items-center justify-content-end">
+              <Icon name="flag" appearance="warning"/>
+              </Column>
+            </Row>
+          <div className="d-flex">
+            <Button className="mr-4" type="submit" appearance="success">Save</Button>
+            <Button>Cancel</Button>
+          </div>
+          </form>
+        </Card>
+      </div>
+      );
+    }
+  }
+
+  return <InlineLabelForm />
+}`;
+
+export default {
+  title: 'Patterns|Forms',
+  parameters: {
+    docs: {
+      docPage: {
+        customCode,
+        title: 'Inline Label Form',
+        noProps: true
+      }
+    }
+  }
+};
---
+++ core/components/patterns/forms/StepperForm.story.tsx
@@ -0,0 +1,215 @@
+import * as React from 'react';
+
+export const stepperForm = () => <></>;
+
+const customCode = `
+// import * as React from 'react';
+// import { Button, Label, Card, Stepper, Text, Heading, DateRangePicker, Dropdown, Link, Paragraph } from 'design-system';
+
+() => {
+  const steps = [
+    {
+      label: 'Define Input/Output',
+      value: 'Define_Input_Output'
+    },
+    {
+      label: 'Add Configuration',
+      value: 'Add_Configuration'
+    },
+  ];
+  const totalSteps = steps.length;
+
+  const options = [];
+  for (let i = 1; i <= 10; i++) {
+    options.push({
+      label: \`Option \${i}\`,
+      value: \`Option \${i}\`,
+    });
+  }
+
+  class StepperForm extends React.Component {
+    constructor(props = {}) {
+      super(props);
+
+      this.state = {
+        activeStep: 0,
+        completedStep: -1,
+        data: {},
+        configuration: {},
+      };
+
+      this.onChangeStep = this.onChangeStep.bind(this);
+      this.onClickNext = this.onClickNext.bind(this);
+      this.onChangeOutput = this.onChangeOutput.bind(this);
+      this.onSubmit = this.onSubmit.bind(this);
+      this.renderButton = this.renderButton.bind(this);
+    }
+
+    onChangeStep(activeStep) {
+      this.setState({
+        activeStep
+      })
+    }
+
+    onChangeOutput(value, name) {
+      const data = {
+        ...this.state.data,
+        [name]: value,
+      };
+
+      this.setState({
+        data
+      });
+    }
+
+    onClickNext() {
+      const { activeStep, completedStep } = this.state;
+      const updatedActive = activeStep > completedStep ? activeStep + 1 : completedStep + 1;
+      this.setState({
+        activeStep: updatedActive,
+        completedStep: activeStep > completedStep ? activeStep : completedStep
+      });
+    }
+
+    onSubmit(event) {
+      event.preventDefault();
+      console.log(this.state.data);
+      console.log(this.state.configuration);
+      return false;
+    }
+
+    renderButton() {
+      if (this.state.activeStep + 1 === totalSteps) {
+        return <Button appearance="success" type="submit">Save</Button>
+      }
+
+      return <Button appearance="primary" type="button" onClick={this.onClickNext}>Next</Button>
+    }
+
+    render() {
+      const { value } = steps[this.state.activeStep];
+
+      return (
+        <div className="w-75">
+          <Card className="px-7 py-6">
+            <form onSubmit={this.onSubmit}>
+              <div className="d-flex justify-content-between">
+                <Stepper
+                  steps={steps}
+                  active={this.state.activeStep}
+                  completed={this.state.completedStep}
+                  onChange={this.onChangeStep}
+                />
+                {this.renderButton()}
+              </div>
+              <div className={value !== 'Define_Input_Output' ? 'd-none' : ''}>
+                <div className="d-flex mr-3 mt-7 mb-2">
+                  <Text weight="strong" className="mr-4">Source</Text>
+                  <Badge appearance="warning">2 Inputs</Badge>
+                </div>
+                <Text size="small" appearance="subtle">
+                  The system automatically creates collection for multiple support.
+                </Text>
+                <div className="w-50 mt-4">
+                  <Dropdown
+                    options={options}
+                    placeholder="Input Collection 1"
+                    className="mb-4"
+                    onChange={(value) => this.onChangeOutput(value, 'collection1')}
+                  />
+                  <Dropdown
+                    options={options}
+                    placeholder="Input Collection 2"
+                    onChange={(value) => this.onChangeOutput(value, 'collection2')}
+                  />
+                </div>
+                <div className="d-flex mr-3 mt-8 mb-2">
+                  <Text weight="strong" className="mr-4">Destination</Text>
+                  <Badge appearance="success">8 Outputs</Badge>
+                </div>
+                <Text size="small" appearance="subtle">
+                  The system automatically creates collection for multiple support.
+                </Text>
+                <div className="w-50 mt-6">
+                  <Label withInput={true}>Destination Collection</Label>
+                  <Dropdown
+                    options={options}
+                    placeholder="Select Destination"
+                    onChange={(value) => this.onChangeOutput(value, 'collection')}
+                  />
+                </div>
+                <div className="mt-6 w-50">
+                  <Label withInput={true} required>Prefix</Label>
+                  <InputMask
+                    mask={[/\\d/, '_', /\\d/, '_', /\\d/]}
+                    name="prefix"
+                    placeholder="<workspace_id>_<workflow_id>_<pipeline_id>"
+                    placeholderChar="-"
+                    onChange={(e) => this.onChangeOutput(e.target.value, e.target.name)}
+                  />
+                </div>
+                <div className="w-25 mt-6">
+                  <Label withInput={true} required>Retention</Label>
+                  <Dropdown options={options} onChange={(value) => this.onChangeOutput(value, 'retention')} />
+                  <Label className="mt-6" withInput={true}>Visibility Clarification</Label>
+                  <Dropdown options={options} onChange={(value) => this.onChangeOutput(value, 'clarification')} />
+                </div>
+              </div>
+              <div className={value !== 'Add_Configuration' ? 'd-none' : ''}>
+                <div className="d-flex mt-7">
+                  <Avatar className="mr-5" firstName="A" appearance="success" />
+                  <div className="d-flex flex-column">
+                    <Text weight="strong" className="mb-2">Job Configuration</Text>
+                    <Text size="small" appearance="subtle">
+                      The system automatically creates collection for multiple support.
+                    </Text>
+                  </div>
+                </div>
+                <div className="w-25 mt-6">
+                  <Label withInput={true} required>Mode</Label>
+                  <Dropdown
+                    options={options}
+                    onChange={(value) => {
+                      this.setState({
+                        configuration: { ...this.state.configuration, mode: value }
+                      });
+                    }}
+                  />
+                </div>
+                <div className="mt-6 w-50">
+                  <Label withInput={true} required>Regex</Label>
+                  <Textarea
+                    rows={3}
+                    onChange={(e, value) => {
+                      this.setState({
+                        configuration: { ...this.state.configuration, regex: value }
+                      });
+                    }}
+                  />
+                </div>
+                <Label className="mt-6" withInput={true} required>Retention</Label>
+                <Switch appearance="primary" defaultChecked={true} />
+              </div>
+            </form>
+          </Card>
+        </div>
+      );
+    }
+
+  }
+
+  return <StepperForm />
+}`;
+
+export default {
+  title: 'Patterns|Forms',
+  parameters: {
+    docs: {
+      docPage: {
+        customCode,
+        title: 'Stepper Form',
+        noProps: true
+      }
+    }
+  }
+};
