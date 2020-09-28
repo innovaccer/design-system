1920a4d6ecc8724acad3c2acfc1e53e1b31fab39
Aditya Kumawat <aditya19496@gmail.com>
2020-09-28 13:13:04 +0530

    docs(Layout): adds patterns

---
+++ core/components/patterns/layouts/miniSidebarTemplate.story.tsx
@@ -0,0 +1,134 @@
+import * as React from 'react';
+
+export const miniSidebarTemplate = () => <></>;
+
+const customCode = `() => {
+  const data = [
+    {
+      name: 'patient_360',
+      label: 'Patient 360',
+      icon: 'assignment_ind',
+      link: '/patient360'
+    },
+    {
+      name: 'care_management',
+      label: 'Care Management and Resources',
+      icon: 'forum',
+      subMenu: [
+        {
+          name: 'care_management.timeline',
+          label: 'Timeline'
+        },
+        {
+          name: 'care_management.care_plans',
+          label: 'Care Plans'
+        }
+      ]
+    },
+    {
+      name: 'episodes',
+      label: 'Episodes',
+      disabled: true,
+      icon: 'airline_seat_flat_angled'
+    },
+    {
+      name: 'risk',
+      label: 'Risk',
+      icon: 'favorite',
+      subMenu: [
+        {
+          name: 'risk.timeline',
+          label: 'Timeline'
+        },
+        {
+          name: 'risk.care_plans',
+          label: 'Care Plans'
+        }
+      ]
+    },
+    {
+      name: 'claims',
+      label: 'Claims',
+      icon: 'receipt'
+    },
+    {
+      name: 'profile',
+      label: 'Profile',
+      icon: 'account_circle'
+    },
+    {
+      name: 'manula_entry',
+      label: 'Manual Entry',
+      icon: 'border_color'
+    },
+    {
+      name: 'documents',
+      label: 'Documents',
+      icon: 'assignment'
+    }
+  ];
+
+  const [active, setActive] = React.useState({
+    name: 'care_management.timeline'
+  });
+  const [expanded, setExpanded] = React.useState(true);
+
+  const onClickHandler = (menu) => {
+    console.log('menu-clicked: ', menu);
+    setActive(menu);
+  };
+
+  return(
+    <Row className="vh-100 bg-secondary-lightest">
+      <div className="d-flex bg-secondary-lighter">
+        <Navigation
+          type="vertical"
+          menus={data}
+          expanded={expanded}
+          footer={true}
+          onToggle={(val) => setExpanded(val)}
+          active={active}
+          onClick={onClickHandler}
+        />
+      </div>
+      <Row>
+        <Column size={8} className="d-flex flex-column h-100 overflow-hidden pb-6">
+          <PageHeader
+            title="Page title"
+            separator={false}
+          />
+          <Row className="px-6 h-100">
+            <Column className="h-100 v-100 bg-light" />
+          </Row>
+        </Column>
+        <Column size={1}>
+          <Row className="px-6 h-100">
+            <Column className="h-100 v-100 bg-light" />
+          </Row>
+        </Column>
+        <Column size={3} className="d-flex flex-column h-100 overflow-hidden pb-6">
+          <PageHeader
+            title="Page title"
+            separator={false}
+          />
+          <Row className="px-6 h-100">
+            <Column className="h-100 v-100 bg-light" />
+          </Row>
+        </Column>
+      </Row>
+    </Row>
+  );
+}`;
+
+export default {
+  title: 'Patterns|Layouts',
+  parameters: {
+    docs: {
+      docPage: {
+        customCode,
+        title: 'Mini sidebar Template',
+        noProps: true
+      }
+    }
+  }
+};
---
+++ core/components/patterns/layouts/resourceTableTemplate.story.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+export const resourceTableTemplate = () => <></>;
+
+const customCode = `() => {
+  return(
+    <div className="d-flex flex-column bg-secondary-lightest vh-100 pb-6 overflow-hidden">
+      <PageHeader
+        title="Page title"
+        separator={false}
+      />
+      <Row className="px-6 h-100">
+        <Column className="h-100 v-100 bg-light" />
+      </Row>
+    </div>
+  );
+}`;
+
+export default {
+  title: 'Patterns|Layouts',
+  parameters: {
+    docs: {
+      docPage: {
+        customCode,
+        title: 'Resource Table Template',
+        noProps: true
+      }
+    }
+  }
+};
