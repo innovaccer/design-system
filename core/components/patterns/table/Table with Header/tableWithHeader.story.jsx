c7c5e4fa8484521aa4e59e660de5e20103abdc9f
Aditya Kumawat <aditya19496@gmail.com>
2020-10-19 16:56:46 +0530

    chore(pattern): adds DraggableDropdown in Table with Header

--- core/components/patterns/table/Table with Header/tableWithHeader.story.jsx
+++ core/components/patterns/table/Table with Header/tableWithHeader.story.jsx
@@ -114,6 +114,144 @@ import './style.css';
   
     return updatedData;
   };
+
+  const moveToIndex = (arr, from, to) => {
+    if (from === to) return arr;
+  
+    let newArr = arr;
+    if (from < to) {
+      newArr = [
+        ...arr.slice(0, from),
+        ...arr.slice(from + 1, to + 1),
+        arr[from],
+        ...arr.slice(to + 1)
+      ];
+    } else {
+      newArr = [
+        ...arr.slice(0, to),
+        arr[from],
+        ...arr.slice(to, from),
+        ...arr.slice(from + 1)
+      ];
+    }
+  
+    return newArr;
+  };
+
+  const DraggableDropdown = (props) => {
+    const {
+      options,
+      onChange
+    } = props;
+  
+    const [open, setOpen] = React.useState(false);
+    const [tempOptions, setTempOptions] = React.useState(options);
+    const [triggerWidth, setTriggerWidth] = React.useState('var(--spacing-8)');
+  
+    React.useEffect(() => {
+      setTempOptions(options);
+    }, [open]);
+  
+    const handleParentChange = (e) => {
+      setTempOptions(tempOptions.map(option => ({ ...option, selected: e.target.checked })));
+    };
+  
+    const handleChildChange = (e, index) => {
+      const newOptions = [...tempOptions];
+      newOptions[index] = {
+        ...newOptions[index],
+        selected: e.target.checked
+      };
+  
+      setTempOptions(newOptions);
+    };
+  
+    const onToggleHandler = (newOpen) => {
+      setOpen(newOpen);
+    };
+  
+    const onCancelHandler = () => {
+      setOpen(false);
+    };
+  
+    const onApplyHandler = () => {
+      setOpen(false);
+  
+      if (onChange) onChange(tempOptions);
+    };
+  
+    return (
+      <div className="Dropdown">
+        <Popover
+          open={open}
+          onToggle={onToggleHandler}
+          trigger={(
+            <Button
+              ref={el => {
+                // setTriggerWidth(\`\${el.clientWidth}px\`);
+              }}
+              size="tiny"
+              appearance="transparent"
+              icon="keyboard_arrow_down_filled"
+              iconAlign="right"
+            >
+              {\`Showing \${options.filter(option => option.selected).length} of \${options.length} columns\`}
+            </Button>
+          )}
+          triggerClass="w-100"
+          customStyle={{
+            width: triggerWidth
+          }}
+          className="Header-draggableDropdown"
+        >
+          <div className="Dropdown-wrapper">
+            <div className="OptionWrapper">
+              <Checkbox
+                className="OptionCheckbox"
+                label="Select All"
+                checked={tempOptions.every(option => option.selected)}
+                indeterminate={tempOptions.some(option => option.selected)
+                  && tempOptions.some(option => !option.selected)}
+                onChange={handleParentChange}
+              />
+            </div>
+            {tempOptions.map((option, index) => {
+              return (
+                <div
+                  key={option.value}
+                  className="OptionWrapper d-flex flex-space-between align-items-center cursor-pointer"
+                  draggable={true}
+                  onDragStart={e => {
+                    e.dataTransfer.setData('index', \`\${index}\`);
+                  }}
+                  onDragOver={e => e.preventDefault()}
+                  onDrop={e => {
+                    const from = +e.dataTransfer.getData('index');
+                    const to = index;
+  
+                    if (from !== to) setTempOptions(moveToIndex(tempOptions, from, to));
+                  }}
+                >
+                  <Checkbox
+                    className="OptionCheckbox"
+                    name={option.value}
+                    label={option.label}
+                    checked={tempOptions[index].selected}
+                    onChange={e => handleChildChange(e, index)}
+                  />
+                  <Icon name="drag_handle" className="mr-4" />
+                </div>
+              );
+            })}
+          </div>
+          <div className="Dropdown-buttonWrapper">
+            <Button className="mr-4" size="tiny" onClick={onCancelHandler}>Cancel</Button>
+            <Button appearance="primary" size="tiny" onClick={onApplyHandler}>Apply</Button>
+          </div>
+        </Popover>
+      </div>
+    );
+  };
   
   const Header = (props) => {
     const {
@@ -174,26 +312,20 @@ import './style.css';
       }
     };
   
-    const onHideColumn = (selected) => {
-      const newSchema = schema.map(s => ({
-        ...s,
-        hidden: selected.findIndex(val => val === s.name) === -1
-      }));
-  
-      if (updateSchema) updateSchema(newSchema);
-    };
-  
-    const pinnedSchema = schema.filter(s => s.pinned);
-    const leftPinnedSchema = pinnedSchema.filter(s => s.pinned === 'left');
-    const rightPinnedSchema = pinnedSchema.filter(s => s.pinned === 'right');
-    const unpinnedSchema = schema.filter(s => !s.pinned);
-    const renderedSchema = [...leftPinnedSchema, ...unpinnedSchema, ...rightPinnedSchema];
-  
-    const columnOptions = renderedSchema.map(s => ({
+    const columnOptions = schema.map(s => ({
       label: s.displayName,
       value: s.name,
       selected: !s.hidden
     }));
+  
+    const onDynamicColumnUpdate = (options) => {
+      const newSchema = options.map(option => ({
+        ...schema.find(colSchema => colSchema.name === option.value),
+        hidden: !option.selected
+      }));
+  
+      if (updateSchema) updateSchema(newSchema);
+    };
   
     const selectedCount = data.filter(d => d._selected).length;
     const label = withCheckbox && selectedCount ?
@@ -286,29 +418,9 @@ import './style.css';
             }
           </div>
           <div className="Header-hideColumns">
-            <Dropdown
-              key={\`\${flag}\`}
-              triggerSize={'tiny'}
-              withCheckbox={true}
-              showApplyButton={true}
+            <DraggableDropdown
               options={columnOptions}
-              totalOptions={columnOptions.length}
-              align={'left'}
-              triggerOptions={{
-                labelLimit: 0,
-                customLabel: (selected, totalOptions) => \`Showing \${selected} of \${totalOptions} columns\`,
-                customTrigger: triggerLabel => (
-                  <Button
-                    size="tiny"
-                    appearance="transparent"
-                    icon="keyboard_arrow_down_filled"
-                    iconAlign="right"
-                  >
-                    {triggerLabel ? triggerLabel : \`Showing 0 of \${columnOptions.length} columns\`}
-                  </Button>
-                )
-              }}
-              onChange={selected => onHideColumn(selected)}
+              onChange={onDynamicColumnUpdate}
             />
           </div>
         </div>
@@ -588,41 +700,40 @@ import './style.css';
               )}
             </Card>
           </div>
-          {showVerticalFilters && (
-            <div className="Table-filters Table-filters--vertical">
-              <div className="Table-filtersHeading">
-                <Subheading>Filters</Subheading>
-                <Icon name="close" className="Table-filtersCloseIcon" onClick={() => this.setState({ showVerticalFilters: false })} />
+          <div className={\`Table-filters Table-filters--vertical\${!showVerticalFilters ? ' d-none' : ''}\`}>
+            <div className="Table-filtersHeading">
+              <Subheading>Filters</Subheading>
+              <Icon name="close" className="Table-filtersCloseIcon" onClick={() => this.setState({ showVerticalFilters: false })} />
+            </div>
+            <div>
+              <div className="Table-filter">
+                <Dropdown
+                  key="gender"
+                  disabled={loading}
+                  withCheckbox={true}
+                  showApplyButton={true}
+                  inlineLabel={"Gender"}
+                  options={[
+                    { label: 'Male', value: 'male', selected: true },
+                    { label: 'Female', value: 'female', selected: true },
+                  ]}
+                  onChange={selected => this.onFilterChange("gender", selected)}
+                />
               </div>
-              <div>
-                <div className="Table-filter">
-                  <Dropdown
-                    key="gender"
-                    disabled={loading}
-                    withCheckbox={true}
-                    showApplyButton={true}
-                    inlineLabel={"Gender"}
-                    options={[
-                      { label: 'Male', value: 'male', selected: true },
-                      { label: 'Female', value: 'female', selected: true },
-                    ]}
-                    onChange={selected => this.onFilterChange("gender", selected)}
-                  />
-                </div>
-                <div className="Table-filter">
-                  <DatePicker
-                    withInput={true}
-                    inputOptions={{
-                      inlineLabel: "Date",
-                      placeholder: "mm/dd/yyyy",
-                      disabled: loading
-                    }}
-                    onDateChange={(_date, dateStr) => this.onFilterChange("date", dateStr)}
-                  />
-                </div>
+              <div className="Table-filter">
+                <DatePicker
+                  withInput={true}
+                  label="Date"
+                  inputOptions={{
+                    placeholder: "mm/dd/yyyy",
+                    disabled: loading,
+                    minWidth: 'unset'
+                  }}
+                  onDateChange={(_date, dateStr) => this.onFilterChange("date", dateStr)}
+                />
               </div>
             </div>
-          )}
+          </div>
         </div>
       );
     }
@@ -855,8 +966,7 @@ import './style.css';
 }`
 
 export default {
-  title: 'Organisms|Grid/Patterns',
-  // component: Grid,
+  title: 'Patterns|Grid',
   parameters: {
     docs: {
       docPage: {
