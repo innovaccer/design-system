f8fcbc700ee61d26fd3d808b4bf527dd1d076b9c
Aditya Kumawat <aditya19496@gmail.com>
2020-12-17 19:44:59 +0530

    feat(Modal, Dialog): updates as per new design guidelines

--- core/components/molecules/modalBody/ModalBody.tsx
+++ core/components/molecules/modalBody/ModalBody.tsx
@@ -2,36 +2,21 @@ import * as React from 'react';
 import classNames from 'classnames';
 import { BaseProps, extractBaseProps } from '@/utils/types';
 
-const { useRef, useEffect, useState } = React;
-
 export interface ModalBodyProps extends BaseProps {
   children: React.ReactNode;
 }
 
 export const ModalBody = (props: ModalBodyProps) => {
-  const [scroll, setScroll] = useState(false);
-  const ref = useRef<HTMLDivElement>(null);
   const { children, className } = props;
 
   const baseProps = extractBaseProps(props);
 
-  useEffect(() => {
-
-    const scrollHeight = ref && ref.current ? ref.current.scrollHeight : 0;
-    const clientHeight = ref && ref.current ? ref.current.clientHeight : 0;
-
-    if (scrollHeight > clientHeight) {
-      setScroll(true);
-    }
-  }, [ref]);
-
   const classes = classNames({
     'Modal-body': true,
-    ['Modal-body--border']: scroll
   }, className);
 
   return (
-    <div data-test="DesignSystem-ModalBody" {...baseProps} className={classes} ref={ref}>
+    <div data-test="DesignSystem-ModalBody" {...baseProps} className={classes}>
       {children}
     </div>
   );
