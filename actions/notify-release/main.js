9958ccfbe7454a6ea63ec4fefa1c153a9aa65af1
Satyam Yadav <satyamyadav3@gmail.com>
2021-01-12 20:16:47 +0530

    feat: adds action to notify new releases through google chat hooks api

---
+++ actions/notify-release/main.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+
+/**
+ * action to push notification to google chat webhook api
+ */
+
+const http = require("https");
+
+const options = {
+  "method": "POST",
+  "hostname": "chat.googleapis.com",
+  "port": null,
+  "path": process.env.GCHAT_PATH,
+  "headers": {
+    "cache-control": "no-cache"
+  }
+};
+
+const req = http.request(options, function (res) {
+  const chunks = [];
+
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function () {
+    const body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+});
+
+const repo = "https://github.com/innovaccer/design-system";
+const version = require(`${process.cwd()}/package.json`).version;
+
+const message = {
+  "cards": [
+    {
+      "sections": [
+        {
+          "widgets": [
+            {
+              "keyValue": {
+                "topLabel": "Design system",
+                "content": `${version}`,
+                "contentMultiline": "false",
+                "bottomLabel": "Released",
+                "onClick": {
+                  "openLink": {
+                    "url": `${repo}/releases/tag/${version}`
+                  }
+                },
+                "icon": "FLIGHT_ARRIVAL",
+                "button": {
+                  "textButton": {
+                    "text": "View Changelog",
+                    "onClick": {
+                      "openLink": {
+                        "url": `${repo}/blob/master/CHANGELOG.md`
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+
+req.write(JSON.stringify(message));
+req.end();
