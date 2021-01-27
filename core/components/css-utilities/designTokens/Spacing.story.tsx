f9216b71d325c9c4b7120d5fd2c774b64db4b09b
Shib00 <shivamd011@gmail.com>
2021-01-27 11:06:26 +0530

    chore(docs): adds design token stories

---
+++ core/components/css-utilities/designTokens/Spacing.story.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+
+import { Card, Heading, Table } from '@/index';
+import { spacingData } from './Data';
+import { spaceSchema } from './Schema';
+
+export const spacing = () => {
+  return (
+    <div>
+      <Heading size="xxl">Design Tokens</Heading>
+      <br />
+      <br />
+      <Heading size="m">Spacing</Heading>
+      <br />
+      <Card className="h-100">
+        <Table data={spacingData} schema={spaceSchema} />
+      </Card>
+    </div>
+  );
+};
+
+export default {
+  title: 'Design Tokens',
+  component: spacing,
+};
