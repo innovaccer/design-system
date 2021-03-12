ca83df72a85e35c8d5450ddaf14ee477a8de13b7
Satyam Yadav <satyamyadav3@gmail.com>
2021-03-12 15:10:55 +0530

    WIP

--- core/components/atoms/avatar/Avatar.tsx
+++ core/components/atoms/avatar/Avatar.tsx
@@ -112,6 +112,7 @@ export const Avatar = (props: AvatarProps) => {
             appearance={'white'}
             className={ContentClass}
           >
+            
             {initials}
           </Text>
         )}
