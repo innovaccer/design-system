4b3b81d431cf0ebf3713568d137dc426d131fecf
Riya Lohia <riyalh1997@gmail.com>
2021-03-18 23:39:55 +0530

    feat(Link): adds size and appearance support

--- core/components/atoms/link/__stories__/index.story.tsx
+++ core/components/atoms/link/__stories__/index.story.tsx
@@ -1,16 +1,22 @@
 import * as React from 'react';
-import { text, select } from '@storybook/addon-knobs';
-import Link from '../index';
+import { text, select, boolean } from '@storybook/addon-knobs';
+import { Link } from '@/index';
 import { action } from '@storybook/addon-actions';
 
 // CSF format story
 export const all = () => {
   const href = text('href', 'http://innovaccer.com');
   const target = select('target', ['_blank', '_self', '_parent', '_top'], '_blank');
+  const size = select('size', ['regular', 'tiny'], 'regular');
+  const appearance = select('appearance', ['default', 'subtle'], 'default');
+  const disabled = boolean('disabled', false);
 
   const options = {
     href,
-    target
+    target,
+    size,
+    appearance,
+    disabled
   };
 
   return (
---
+++ core/components/atoms/link/__stories__/variants/Appearance.story.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { Link } from '@/index';
+
+// CSF format story
+export const appearance = () => {
+  return (
+    <div className="d-flex">
+      <Link appearance="default" href="http://innovaccer.com">
+        Default Link.
+      </Link>
+      <Link appearance="subtle" href="http://innovaccer.com" className="ml-7">
+        Subtle Link.
+      </Link>
+    </div>
+  );
+};
+
+export default {
+  title: 'Atoms|Typography/Link/Variants',
+  component: Link,
+  parameters: {
+    docs: {
+      docPage: {
+        title: 'Link'
+      }
+    }
+  }
+};
