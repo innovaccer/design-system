91a51c90fff2f798f0cc962c8830ce314b47ddfa
Riya Lohia <riyalh1997@gmail.com>
2021-04-23 20:58:52 +0530

    feat(Card): adds CardHeader, CardBody and CardFooter

---
+++ core/components/atoms/cardHeader/CardHeader.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import classNames from 'classnames';
+import { BaseProps, extractBaseProps } from '@/utils/types';
+
+export interface CardHeaderProps extends BaseProps {
+  /**
+   * React component to be rendered inside `CardHeader`
+   */
+  children: React.ReactNode;
+}
+
+export const CardHeader = (props: CardHeaderProps) => {
+  const { className, children } = props;
+  const baseProps = extractBaseProps(props);
+
+  const classes = classNames({
+    'Card-header': true,
+  }, className);
+
+  return (
+    <div data-test="DesignSystem-CardHeader" {...baseProps} className={classes}>
+      {children}
+    </div>
+  );
+};
+
+CardHeader.displayName = 'CardHeader';
+
+export default CardHeader;
