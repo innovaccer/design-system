ef36b3ac783e8ba6609335f1752918226d1ddc82
Satyam Yadav <satyamyadav3@gmail.com>
2021-04-25 17:59:11 +0530

    docs(message): updates message component stories according to new documentation

--- core/components/atoms/message/__stories__/Info.story.tsx
+++ core/components/atoms/message/__stories__/Info.story.tsx
@@ -1,21 +1,19 @@
 import * as React from 'react';
-import { Message, Text } from '@/index';
+import { Message, Row, Column } from '@/index';
 
 // CSF format story
-export const info = () => (
-  <Message
-    appearance={'info'}
-    description="Reevaluate the entry after submission"
-    actions={(
-      <>
-        <Text className="cursor-pointer" appearance="link">Action 1</Text>
-        <Text className="ml-5 cursor-pointer" appearance="link">Action 2</Text>
-      </>
-    )}
-  />
+export const infoMessage = () => (
+  <Row>
+    <Column size="4">
+      <Message
+        appearance="info"
+        description="Patient profile has been updated with new records."
+      />
+    </Column>
+  </Row>
 );
 
 export default {
-  title: 'Components/Message/Info',
+  title: 'Components/Message/Info Message',
   component: Message
 };
