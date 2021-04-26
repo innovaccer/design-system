a0f1f7b8ec0ecc4055ad8a42743d5cc5f2834e21
Satyam Yadav <satyamyadav3@gmail.com>
2021-04-26 19:34:04 +0530

    feat(release): removes script step to append gitlog in CHANGELOG.md file, keeps git log printing
    The change log needs to be written and reviewd manualy, the change removes the ability to
    automaticaly append git log as change log.

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,12 +3,11 @@ PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 DATE=$(date +%F)
 
 # Append changelog
-echo "\n-------------------\n **v$PACKAGE_VERSION**  ($DATE) \n\n$LOG" > CHANGELOG.md &&
+echo "\n-------------------\n **v$PACKAGE_VERSION**  ($DATE) \n\n$LOG";
 
 # build
 npm run build && 
 
 # add changes to git
-git add CHANGELOG.md &&
 git add dist -f && 
 git add css/dist -f
