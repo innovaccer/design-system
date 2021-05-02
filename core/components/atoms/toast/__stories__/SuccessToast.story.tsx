b9e20b789338575053427218afe8132f1fa50db7
Riya Lohia <riyalh1997@gmail.com>
2021-05-02 20:46:52 +0530

    chore(Toast): adds stories

---
+++ core/components/atoms/toast/__stories__/SuccessToast.story.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Toast } from '@/index';
+
+// CSF format story
+export const successToast = () => (
+  <Toast
+    appearance="success"
+    title="Outreach successfully sent"
+    message="2,340 outreach messages have been successfully sent."
+  />
+);
+
+export default {
+  title: 'Components/Toast/Success Toast',
+  component: Toast
+};
