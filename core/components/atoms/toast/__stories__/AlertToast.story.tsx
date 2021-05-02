4535f852632a2b89516cc9e1bb77844878059f11
Riya Lohia <riyalh1997@gmail.com>
2021-05-02 20:46:52 +0530

    chore(Toast): adds stories

---
+++ core/components/atoms/toast/__stories__/AlertToast.story.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { Toast } from '@/index';
+
+// CSF format story
+export const alertToast = () => (
+  <Toast
+    appearance="alert"
+    title="Campaign failed to run"
+    message="Try to run again. If it continues to fail, please raise a ticket."
+  />
+);
+
+export default {
+  title: 'Components/Toast/Alert Toast',
+  component: Toast
+};
