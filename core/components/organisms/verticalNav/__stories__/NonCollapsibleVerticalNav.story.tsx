cf92fcfd63880c3b8590747156ad24cb540036d5
Riya Lohia <riyalh1997@gmail.com>
2021-05-03 16:36:51 +0530

    chore(verticalNav): adds stories

---
+++ core/components/organisms/verticalNav/__stories__/NonCollapsibleVerticalNav.story.tsx
@@ -0,0 +1,95 @@
+import * as React from 'react';
+import { VerticalNav } from '@/index';
+
+export const nonCollapsibleVerticalNavigation = () => {
+
+  const data = [
+    {
+      name: 'practice',
+      label: 'Practice',
+    },
+    {
+      name: 'users',
+      label: 'Users',
+    },
+    {
+      name: 'copayment',
+      label: 'Copayment',
+    },
+    {
+      name: 'subscription',
+      label: 'Subscription',
+    },
+    {
+      name: 'import_export_settings',
+      label: 'Import/Export Settings',
+    },
+  ];
+
+  const [active, setActive] = React.useState({
+    name: 'users'
+  });
+
+  return (
+    <div style={{ height: '50vh', background: 'var(--secondary-lightest)' }}>
+      <VerticalNav
+        menus={data}
+        active={active}
+        expanded={true}
+        onClick={setActive}
+      />
+    </div>
+  );
+};
+
+const customCode = `() => {
+  const data = [
+    {
+      name: 'practice',
+      label: 'Practice',
+    },
+    {
+      name: 'users',
+      label: 'Users',
+    },
+    {
+      name: 'copayment',
+      label: 'Copayment',
+    },
+    {
+      name: 'subscription',
+      label: 'Subscription',
+    },
+    {
+      name: 'import_export_settings',
+      label: 'Import/Export Settings',
+    },
+  ];
+
+  const [active, setActive] = React.useState({
+    name: 'users'
+  });
+
+  return (
+    <div style={{ height: '50vh', background: 'var(--secondary-lightest)' }}>
+      <VerticalNav
+        menus={data}
+        active={active}
+        expanded={true}
+        onClick={setActive}
+      />
+    </div>
+  );
+}`;
+
+export default {
+  title: 'Components/VerticalNav/Non Collapsible Vertical Navigation',
+  component: VerticalNav,
+  parameters: {
+    docs: {
+      docPage: {
+        customCode
+      }
+    }
+  }
+};
