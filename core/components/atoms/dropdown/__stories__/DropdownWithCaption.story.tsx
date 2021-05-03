d34dfc1232ba37834df2b06767fe245898dd5027
Riya Lohia <riyalh1997@gmail.com>
2021-05-03 12:37:29 +0530

    chore(Dropdown): adds stories

---
+++ core/components/atoms/dropdown/__stories__/DropdownWithCaption.story.tsx
@@ -0,0 +1,77 @@
+import * as React from 'react';
+import { Dropdown } from '@/index';
+import { Uncontrolled, Controlled } from './_common_/types';
+import Label from '../../label';
+import Caption from '../../caption';
+
+const options = [
+  {
+    label: 'Alabama (205)',
+    value: 'Alabama (205)',
+  },
+  {
+    label: 'Alabama (251)',
+    value: 'Alabama (251)',
+  },
+  {
+    label: 'Alabama (256)',
+    value: 'Alabama (256)',
+  },
+  {
+    label: 'Alabama (334)',
+    value: 'Alabama (334)',
+  },
+  {
+    label: 'Alabama (938)',
+    value: 'Alabama (938)',
+  },
+  {
+    label: 'Arizona (520)',
+    value: 'Arizona (520)',
+  },
+  {
+    label: 'California (209)',
+    value: 'California (209)',
+  },
+  {
+    label: 'California (408)',
+    value: 'California (408)',
+  },
+  {
+    label: 'Colorado (719)',
+    value: 'Colorado (719)',
+  },
+  {
+    label: 'Connecticut (860)',
+    value: 'Connecticut (860)',
+  }
+];
+
+// CSF format story
+export const dropdownWithCaption = () => (
+  <>
+    <Label withInput={true}>Area code</Label>
+    <Dropdown
+      options={options}
+      withSearch={true}
+      className="w-25"
+      placeholder="Select an area code"
+    />
+    <Caption withInput={true}>If the number with this code is not available, we will use the next best match</Caption>
+  </>
+);
+
+export default {
+  title: 'Components/Dropdown/Dropdown With Caption',
+  component: Dropdown,
+  parameters: {
+    docs: {
+      docPage: {
+        title: 'Dropdown',
+        props: {
+          components: { Uncontrolled, Controlled },
+        }
+      }
+    }
+  }
+};
--- core/components/atoms/dropdown/__stories__/Menu.story.tsx
+++ core/components/atoms/dropdown/__stories__/Menu.story.tsx
@@ -4,29 +4,41 @@ import { Uncontrolled, Controlled } from './_common_/types';
 
 const options = [
   {
-    label: 'Male',
-    value: 'Male',
+    label: 'Edit',
+    value: 'edit',
   },
   {
-    label: 'Female',
-    value: 'Female',
+    label: 'Export',
+    value: 'export',
+  },
+  {
+    label: 'Delete',
+    value: 'delete',
   }
 ];
 
 // CSF format story
-export const menu = () => (
-  <div className="ml-11 mb-10">
+export const dropdownAsMenu = () => (
+  <div className="d-flex w-50">
+    <Dropdown
+      options={options}
+      menu={true}
+      className="mr-5"
+      align="right"
+      maxWidth={130}
+    />
     <Dropdown
       options={options}
       menu={true}
       align="left"
+      className="mr-5"
       maxWidth={130}
     />
   </div>
 );
 
 export default {
-  title: 'Components/Dropdown/Menu',
+  title: 'Components/Dropdown/Dropdown As Menu',
   component: Dropdown,
   parameters: {
     docs: {
