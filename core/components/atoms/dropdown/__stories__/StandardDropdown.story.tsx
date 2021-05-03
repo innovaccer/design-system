70329569b6c32459237926dea5897ea7b5f05101
Riya Lohia <riyalh1997@gmail.com>
2021-05-03 12:37:29 +0530

    chore(Dropdown): adds stories

---
+++ core/components/atoms/dropdown/__stories__/StandardDropdown.story.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { Dropdown } from '@/index';
+import { Uncontrolled, Controlled } from './_common_/types';
+
+const options = [
+  {
+    label: 'Draft',
+    value: 'draft',
+  },
+  {
+    label: 'In Progress',
+    value: 'in_progress',
+  },
+  {
+    label: 'Sent',
+    value: 'sent',
+  },
+  {
+    label: 'Scheduled',
+    value: 'scheduled',
+  },
+  {
+    label: 'Partially Failed',
+    value: 'partially_failed',
+  },
+  {
+    label: 'Completely Failed',
+    value: 'completely_failed',
+  }
+];
+
+// CSF format story
+export const standardDropdown = () => (
+  <Dropdown
+    options={options}
+    className="w-25"
+    placeholder="Status"
+  />
+);
+
+export default {
+  title: 'Components/Dropdown/Standard Dropdown',
+  component: Dropdown,
+  parameters: {
+    docs: {
+      docPage: {
+        title: 'Dropdown',
+        props: {
+          components: { Uncontrolled, Controlled },
+        }
+      }
+    }
+  }
+};
