4f3c685a871661832e1338f950c353c1104b39e4
Satyam Yadav <satyamyadav3@gmail.com>
2021-05-07 19:38:07 +0530

    docs(radio): updates stories according to updated documentation

---
+++ core/components/atoms/radio/__stories__/Overflow.story.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import Radio from '../index';
+
+export const OverflowContent = () => (
+  <div className="w-50">
+    <Radio
+      label="Share both your healthcare data and some personal information."
+      name="options"
+      value="Option 1"
+    />
+  </div>
+);
+
+export default {
+  title: 'Components/Radio/Overflow Content',
+  component: Radio,
+  parameters: {
+    docs: {
+      docPage: {
+        props: { exclude: ['key'] }
+      }
+    }
+  }
+};
