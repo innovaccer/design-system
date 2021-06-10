a179780c66409c224cd58d2a5352d82582fa4a32
Aditya Kumawat <aditya19496@gmail.com>
2021-06-10 10:48:23 +0530

    test(Grid): adds tests

--- core/components/organisms/grid/GridCell.tsx
+++ core/components/organisms/grid/GridCell.tsx
@@ -15,16 +15,19 @@ export interface CellData {
   statusAppearance?: StatusHintProps['appearance'];
 }
 
-// export interface ImageProps {
-//   src: string;
-//   size?: number;
-// }
-
-// export interface StatusProps extends PartialCellProps {
-//   appearanceMapper?: GridProps['statusMapper'];
-// }
-
-export interface PartialCellProps {
+export interface GridCellProps {
+  /**
+   * Grid size
+   */
+  size: GridSize;
+  /**
+   * Row index for cell
+   */
+  rowIndex: number;
+  /**
+   * Column index for cell
+   */
+  colIndex: number;
   /**
    * Row data
    */
@@ -36,65 +39,11 @@ export interface PartialCellProps {
   /**
    * Loading state of cell
    */
-  loading?: boolean;
+  loading: boolean;
   /**
    * Expanded state of row
    */
   expanded?: boolean;
-}
-
-// export const Image = (props: ImageProps) => {
-//   const {
-//     src,
-//     size
-//   } = props;
-
-//   const style = {
-//     height: `${size}px`,
-//     width: `${size}px`,
-//   };
-
-//   return (
-//     <img className="Image" src={src} style={style} />
-//   );
-// };
-
-// const ImageCell = (props: PartialCellProps) => {
-//   const {
-//     schema,
-//     loading
-//   } = props;
-
-//   const src = schema.image;
-//   const size = 32;
-
-//   if (loading) {
-//     return (
-//       <Placeholder imageSize={'medium'} round={true} />
-//     );
-//   }
-
-//   return (
-//     <Image
-//       src={src || ''}
-//       size={size}
-//     />
-//   );
-// };
-
-export interface GridCellProps extends PartialCellProps {
-  /**
-   * Grid size
-   */
-  size?: GridSize;
-  /**
-   * Row index for cell
-   */
-  rowIndex?: number;
-  /**
-   * Column index for cell
-   */
-  colIndex?: number;
 }
 
 type CellProps = {
@@ -212,7 +161,6 @@ export const GridCell = (props: GridCellProps) => {
   const {
     size,
     schema,
-    // rowIndex,
     loading,
   } = props;
 
@@ -227,112 +175,119 @@ export const GridCell = (props: GridCellProps) => {
 
   const cellData = data[name];
 
-  const cellClass = classNames({
-    ['GridCell']: true,
-  });
+  const wrapper = React.useCallback((className: string, children: React.ReactNode) => {
+    const classes = classNames({
+      ['GridCell']: true,
+    }, className);
+
+    return (
+      <div data-test="DesignSystem-GridCell" className={classes}>
+        {children}
+      </div>
+    );
+  }, []);
 
   switch (cellType) {
     case 'DEFAULT':
-      return (
-        <div className={`${cellClass} GridCell--align-${align} GridCell--default`} >
+      return wrapper(`GridCell--${align}Align GridCell--default`, (
+        <>
           {loading ? (
             <PlaceholderParagraph length="medium" />
           ) : (
-              renderTitle({ tooltip, cellData })
-            )
-          }
-        </div>
-      );
+            renderTitle({ tooltip, cellData })
+          )}
+        </>
+      ));
 
     case 'WITH_META_LIST':
-      return (
-        <div className={`${cellClass} GridCell--metaList`} >
+      return wrapper('GridCell--metaList', (
+        <>
           {loading ? (
             <>
               <PlaceholderParagraph length="medium" />
               <PlaceholderParagraph length="large" size="xxs" />
             </>
           ) : (
-              <>
-                {renderTitle({ tooltip, cellData })}
-                {renderMetaList({ cellData })}
-              </>
-            )
-          }
-        </div>
-      );
+            <>
+              {renderTitle({ tooltip, cellData })}
+              {renderMetaList({ cellData })}
+            </>
+          )}
+        </>
+      ));
 
     case 'AVATAR':
-      if (loading) {
-        return (
-          <Placeholder className={`GridCell--align-${align}`} imageSize={'medium'} round={true} />
-        );
-      }
-      return (
-        <div className={`${cellClass} GridCell--align-${align} GridCell--avatar`} >
-          {size !== 'tight' && renderAvatar({ cellData })}
-        </div>
-      );
+      return wrapper(`GridCell--${align}Align GridCell--avatar`, (
+        <>
+          {loading ? (
+            <Placeholder className={`GridCell--${align}Align`} imageSize={'medium'} round={true} />
+          ) : (
+            <>
+              {size !== 'tight' && renderAvatar({ cellData })}
+            </>
+          )}
+        </>
+      ));
 
     case 'AVATAR_WITH_TEXT':
-      if (loading) {
-        return (
-          <Placeholder imageSize={'medium'} round={true}>
-            <PlaceholderParagraph className="ml-3" length="medium" />
-          </Placeholder>
-        );
-      }
-      return (
-        <div className={`${cellClass} GridCell--avatarWithText`} >
-          {size !== 'tight' && renderAvatar({ cellData })}
-          {renderTitle({ tooltip, cellData })}
-        </div>
-      );
+      return wrapper('GridCell--avatarWithText', (
+        <>
+          {loading ? (
+            <Placeholder imageSize={'medium'} round={true}>
+              <PlaceholderParagraph className="ml-3" length="medium" />
+            </Placeholder>
+          ) : (
+            <>
+              {size !== 'tight' && renderAvatar({ cellData })}
+              {renderTitle({ tooltip, cellData })}
+            </>
+          )}
+        </>
+      ));
 
     case 'AVATAR_WITH_META_LIST':
-      if (loading) {
-        return (
-          <Placeholder imageSize={'medium'} round={true}>
-            <PlaceholderParagraph className="ml-3" length="medium" />
-            <PlaceholderParagraph className="ml-3" length="large" />
-          </Placeholder>
-        );
-      }
-      return (
-        <div className={`${cellClass} GridCell--avatarWithText`} >
-          {size !== 'tight' && renderAvatar({ cellData })}
-          <div className="GridCell-metaListWrapper">
-            {renderTitle({ tooltip, cellData })}
-            {renderMetaList({ cellData })}
-          </div>
-        </div>
-      );
+      return wrapper('GridCell--avatarWithMetaList', (
+        <>
+          {loading ? (
+            <Placeholder imageSize={'medium'} round={true}>
+              <PlaceholderParagraph className="ml-3" length="medium" />
+              <PlaceholderParagraph className="ml-3" length="large" />
+            </Placeholder>
+          ) : (
+            <>
+              {size !== 'tight' && renderAvatar({ cellData })}
+              <div className="GridCell-metaListWrapper">
+                {renderTitle({ tooltip, cellData })}
+                {renderMetaList({ cellData })}
+              </div>
+            </>
+          )}
+        </>
+      ));
 
     case 'ICON':
-      if (loading) {
-        return (
-          <Placeholder className={`GridCell--align-${align}`} imageSize={'small'} round={true} />
-        );
-      }
-      return (
-        <div className={`${cellClass} GridCell--align-${align} GridCell--icon`} >
-          {renderIcon({ cellData })}
-        </div>
-      );
+      return wrapper(`GridCell--${align}Align GridCell--icon`, (
+        <>
+          {loading ? (
+            <Placeholder className={`GridCell--${align}Align`} imageSize={'small'} round={true} />
+          ) : (
+            renderIcon({ cellData })
+          )}
+        </>
+      ));
 
     case 'STATUS_HINT':
-      return (
-        <div className={`${cellClass} GridCell--align-${align} GridCell--statusHint`} >
+      return wrapper(`GridCell--${align}Align GridCell--statusHint`, (
+        <>
           {loading ? (
             <Placeholder className="w-75 flex-grow-0" imageSize={'small'} round={true}>
               <PlaceholderParagraph length="large" />
             </Placeholder>
           ) : (
-              renderStatusHint({ cellData })
-            )
-          }
-        </div>
-      );
+            renderStatusHint({ cellData })
+          )}
+        </>
+      ));
   }
 
   return null;
