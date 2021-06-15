f57f29f930b98576eb23567dbb32aa0ab35e45f3
aditya <aditya.jhajharia25@gmail.com>
2021-06-15 12:35:32 +0530

    docs(loaders):adds placeholder story

---
+++ core/components/molecules/placeholder/__stories__/skeletonLoaders.story.tsx
@@ -0,0 +1,40 @@
+import * as React from 'react';
+import { PlaceholderParagraph, Placeholder, Card } from '@/index';
+
+export const skeletonLoaders = () => {
+
+  return (
+    <Card>
+      <div className="px-6 py-5">
+        <Placeholder  round={true} imageSize="large">
+          <PlaceholderParagraph  length="large" size="m" />
+          <PlaceholderParagraph size="m"/>
+        </Placeholder>
+      </div>
+      <div  style={{ borderTop: 'var(--border)' }} className="px-6 py-5">
+        <Placeholder round={true} imageSize="large">
+          <PlaceholderParagraph  length="large" size="m" />
+          <PlaceholderParagraph size="m"/>
+        </Placeholder>
+      </div>
+      <div style={{ borderTop: 'var(--border)' }} className="px-6 py-5">
+        <Placeholder round={true} imageSize="large">
+          <PlaceholderParagraph  length="large" size="m" />
+          <PlaceholderParagraph size="m"/>
+        </Placeholder>
+      </div>
+      <div style={{ borderTop: 'var(--border)' }} className="px-6 py-5">
+        <Placeholder round={true} imageSize="large">
+          <PlaceholderParagraph  length="large" size="m" />
+          <PlaceholderParagraph size="m"/>
+        </Placeholder>
+      </div>
+    </Card>
+  );
+};
+
+export default {
+  title: 'Components/Loaders/Placeholder/Skeleton Loaders',
+  component: Placeholder,
+  subcomponents: { PlaceholderParagraph }
+};
