8c4e3a1804ea201a9723d0685dd96b4b69655748
Sidharth <sidharth.satapathy9@gmail.com>
2021-07-19 11:31:12 +0530

    Removes dist folder.

--- dist/core/components/organisms/grid/GridContext.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { GridProps } from "../../../index.type";
-import { GridRef } from "./Grid";
-declare type ContextProps = GridProps & {
-    ref: GridRef;
-};
-declare const context: React.Context<ContextProps>;
-export declare const GridProvider: React.Provider<ContextProps>;
-export default context;
