5d5c2fecb1eb1ebdddc7829ff0e082e6b6f5f0f7
Vikas Kumar Singh <I1356@IT001843.local>
2021-08-04 16:40:26 +0530

    test(Slider):adds tests for slider component

---
+++ core/components/atoms/slider/__tests__/Slider.test.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import { Slider } from '@/index';
+import { SliderProps as Props } from '../Slider';
+import { testHelper, filterUndefined, valueHelper, testMessageHelper } from '@/utils/testHelper';
+
+const defaultValue = 10;
+const value = 10;
+
+describe('Slider component', () => {
+  const mapper = {
+    defaultValue: valueHelper(defaultValue, { required: true, iterate: true }),
+  };
+
+  const testFunc = (props: Record<number, any>): void => {
+    const attr = filterUndefined(props) as Props;
+
+    it(testMessageHelper(attr), () => {
+      const { baseElement } = render(
+        <Slider
+          {...attr}
+        />
+      );
+      expect(baseElement).toMatchSnapshot();
+    });
+  };
+
+  testHelper(mapper, testFunc);
+});
+
+describe('Slider component', () => {
+  const mapper = {
+    value: valueHelper(value, { required: true, iterate: true }),
+  };
+
+  const testFunc = (props: Record<number, any>): void => {
+    const attr = filterUndefined(props) as Props;
+
+    it(testMessageHelper(attr), () => {
+      const { baseElement } = render(
+        <Slider
+          {...attr}
+        />
+      );
+      expect(baseElement).toMatchSnapshot();
+    });
+  };
+
+  testHelper(mapper, testFunc);
+});
