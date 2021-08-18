1d41c6cc4e828d19770bbee78afe9864c2c3ecbe
Vikas Kumar Singh <I1356@IT001843.local>
2021-08-18 17:20:23 +0530

    feat(Checkbox): adds helptext in checkbox

--- core/components/atoms/checkbox/__tests__/Checkbox.test.tsx
+++ core/components/atoms/checkbox/__tests__/Checkbox.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Checkbox, { CheckboxProps as Props } from '../Checkbox';
 import { testHelper, filterUndefined, valueHelper, testMessageHelper } from '@/utils/testHelper';
 
@@ -14,20 +14,18 @@ describe('Checkbox component', () => {
     size: valueHelper(size, { required: true, iterate: true }),
     checked: valueHelper(true, { required: true }),
     label: valueHelper(StringValue, { required: true }),
+    helpText: valueHelper(StringValue, { required: true }),
     name: valueHelper(StringValue, { required: true }),
     value: valueHelper(StringValue, { required: true }),
     onChange: valueHelper(FunctionValue, { required: true }),
   };
-
   const testFunc = (props: Record<string, any>): void => {
     const attr = filterUndefined(props) as Props;
-
     it(testMessageHelper(attr), () => {
       const { baseElement } = render(<Checkbox {...attr} />);
       expect(baseElement).toMatchSnapshot();
     });
   };
-
   testHelper(mapper, testFunc);
 });
 
@@ -36,20 +34,18 @@ describe('Checkbox component', () => {
     checked: valueHelper(BooleanValue, { required: true, iterate: true }),
     disabled: valueHelper(BooleanValue, { required: true, iterate: true }),
     label: valueHelper(StringValue, { required: true }),
+    helpText: valueHelper(StringValue, { required: true }),
     name: valueHelper(StringValue, { required: true }),
     value: valueHelper(StringValue, { required: true }),
     onChange: valueHelper(FunctionValue, { required: true }),
   };
-
   const testFunc = (props: Record<string, any>): void => {
     const attr = filterUndefined(props) as Props;
-
     it(testMessageHelper(attr), () => {
       const { baseElement } = render(<Checkbox {...attr} />);
       expect(baseElement).toMatchSnapshot();
     });
   };
-
   testHelper(mapper, testFunc);
 });
 
@@ -58,11 +54,11 @@ describe('Checkbox component', () => {
     indeterminate: valueHelper(true, { required: true }),
     disabled: valueHelper(BooleanValue, { required: true, iterate: true }),
     label: valueHelper(StringValue, { required: true }),
+    helpText: valueHelper(StringValue, { required: true }),
     name: valueHelper(StringValue, { required: true }),
     value: valueHelper(StringValue, { required: true }),
     onChange: valueHelper(FunctionValue, { required: true }),
   };
-
   const testFunc = (props: Record<string, any>): void => {
     const attr = filterUndefined(props) as Props;
 
@@ -71,6 +67,48 @@ describe('Checkbox component', () => {
       expect(baseElement).toMatchSnapshot();
     });
   };
-
   testHelper(mapper, testFunc);
+});
+
+describe('Checkbox component', () => {
+  it('renders children', () => {
+    const { getByTestId } = render(<Checkbox label={StringValue} />);
+    expect(getByTestId('DesignSystem-Checkbox')).toBeInTheDocument();
+    expect(getByTestId('DesignSystem-Checkbox-OuterWrapper')).toBeInTheDocument();
+  });
+
+  it('renders children : input', () => {
+    const { getByTestId } = render(<Checkbox label={StringValue} />);
+    expect(getByTestId('DesignSystem-Checkbox-InputBox')).toBeInTheDocument();
+  });
+
+  it('renders children with label prop', () => {
+    const { getByTestId } = render(<Checkbox label={StringValue} />);
+    expect(getByTestId('DesignSystem-Checkbox-InputBox')).toBeInTheDocument();
+    expect(getByTestId('DesignSystem-Checkbox-Label')).toBeInTheDocument();
+    expect(getByTestId('DesignSystem-Checkbox-Label').textContent).toMatch(StringValue);
+  });
+
+  it('renders children with helpText prop', () => {
+    const { getByTestId } = render(<Checkbox label={StringValue} helpText={StringValue} />);
+    expect(getByTestId('DesignSystem-Checkbox-InputBox')).toBeInTheDocument();
+    expect(getByTestId('DesignSystem-Checkbox-Label')).toBeInTheDocument();
+    expect(getByTestId('DesignSystem-Checkbox-HelpText')).toBeInTheDocument();
+    expect(getByTestId('DesignSystem-Checkbox-HelpText').textContent).toMatch(StringValue);
+  });
+
+  it('calls onchange handler', () => {
+    const { getByTestId } = render(<Checkbox onChange={FunctionValue} label={StringValue} helpText={StringValue} />);
+    const input = getByTestId('DesignSystem-Checkbox-InputBox');
+    fireEvent.click(input);
+    expect(FunctionValue).toHaveBeenCalled();
+  });
+
+  it('calls onchange handler when box is checked', () => {
+    const { getByTestId } = render(<Checkbox onChange={FunctionValue} label={StringValue} helpText={StringValue} />);
+    const input = getByTestId('DesignSystem-Checkbox-InputBox');
+    expect(input).not.toBeChecked();
+    fireEvent.change(input, { target: { checked: true } });
+    expect(input).toBeChecked();
+  });
 });
