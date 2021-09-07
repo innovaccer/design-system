933c9527e80500e247e037d05ddfbe4a1d757af0
anuradha9712 <anuradha.mcs18.du@gmail.com>
2021-09-07 22:32:21 +0530

    feat(InlineMessage): adds InlineMessage Component

---
+++ core/components/organisms/inlineMessage/__stories__/Info.story.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { InlineMessage, Row, Column } from '@/index';
+
+// CSF format story
+export const infoMessage = () => (
+  <Row>
+    <Column size="4">
+      <InlineMessage
+        appearance="info"
+        description="There are two new referral requests."
+      />
+    </Column>
+  </Row>
+);
+
+export default {
+  title: 'Components/InlineMessage/Info Message',
+  component: InlineMessage
+};
---
+++ core/components/organisms/inlineMessage/__stories__/Success.story.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { InlineMessage, Row, Column } from '@/index';
+
+// CSF format story
+export const successMessage = () => (
+  <Row>
+    <Column size="4">
+      <InlineMessage
+        appearance="success"
+        description="Inline message goes here."
+      />
+    </Column>
+  </Row>
+);
+
+export default {
+  title: 'Components/InlineMessage/Success Message',
+  component: InlineMessage
+};
