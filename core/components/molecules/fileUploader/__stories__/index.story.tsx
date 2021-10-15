509a9053399d8508cc6ccfdcc4a8a9f7f7bb6d32
Przemyslaw Tyczynski <przemyslaw.tyczynski@allegro.pl>
2021-10-15 15:50:00 +0200

    docs: make `FileUploader` component in storybook interactive
    - Random error while uploading files
    - Progress effect
    - Complete when progress reach 100%

--- core/components/molecules/fileUploader/__stories__/index.story.tsx
+++ core/components/molecules/fileUploader/__stories__/index.story.tsx
@@ -2,34 +2,101 @@ import * as React from 'react';
 import { text, boolean } from '@storybook/addon-knobs';
 import { FileUploader, FileUploaderList, Link } from '@/index';
 import { FileUploaderComponent, FileUploaderListComponent } from './_common_/types';
+import { FileItem } from '../FileUploaderItem';
 
 export const all = () => {
-  const [fileNames, setFileNames] = React.useState<any>([]);
+  const [fileNames, setFileNames] = React.useState<FileItem[]>([]);
+  const [errorOccured, setErrorOccured] = React.useState(false);
 
   const title = text('title', 'Upload files');
-  const formatLabel = text('formatLabel', 'Accepted formats: PDF, jpg');
+  const formatLabel = text('formatLabel', 'Accepted formats: pdf, jpg, jpeg, png');
   const sizeLabel = text('sizeLabel', 'Maximum size: 25 MB');
   const multiple = boolean('multiple', true);
 
-  const onChangeHandler = (fileList: File[]) => {
-    const files = fileList.map((file) => {
-      return {
-        file,
-        status: 'completed',
-      };
-    });
+  const randomBetween = (value: number) => Math.round(Math.random() * value);
 
-    setFileNames(files);
+  const imitateUploading = (fileItem: FileItem) => {
+    const randomProgressChunks = [5, 8, 13];
+    const randomChunkIndex = randomBetween(randomProgressChunks.length - 1);
+    const progressChunk = randomProgressChunks[randomChunkIndex];
+    let baseProgress = fileItem.progress || 0;
+
+    const increment = () => {
+      baseProgress += progressChunk;
+      setProgressFileName(fileItem, baseProgress);
+
+      if (baseProgress < 100) {
+        setTimeout(() => increment(), 100);
+      }
+    };
+
+    increment();
+  };
+
+  const setProgressFileName = (fileName: FileItem, progress = 0) => {
+    setFileNames((previousFileNames) => {
+      const fileNameIndex = previousFileNames.findIndex((fn) => fn.file === fileName.file);
+
+      if (fileNameIndex === -1) {
+        return [...previousFileNames, fileName];
+      }
+
+      return previousFileNames.map((previousFileName, index) => {
+        if (fileNameIndex !== index) return previousFileName;
+
+        return {
+          ...previousFileName,
+          progress,
+          status: progress >= 100 ? 'completed' : 'uploading',
+        };
+      });
+    });
+  };
+
+  const uploadFiles = (fileList: File[]) => {
+    if (!errorOccured && (fileList.length > 1 || fileNames.length > 0)) {
+      const randomErrorIndex = randomBetween(fileList.length - 1);
+      setFileNames((previousFileNames) => [
+        ...previousFileNames,
+        { file: fileList[randomErrorIndex], status: 'error' },
+      ]);
+      setErrorOccured(true);
+
+      if (fileList.length <= 1) {
+        return;
+      }
+
+      fileList.splice(randomErrorIndex, 1);
+    }
+
+    fileList.forEach((file) => {
+      const fileItem: FileItem = {
+        file,
+        status: 'uploading',
+        progress: 0,
+      };
+
+      imitateUploading(fileItem);
+    });
+  };
+
+  const retryUploadFile = (file: File) => {
+    uploadFiles([file]);
+  };
+
+  const deleteUploadedFile = (file: File) => {
+    setFileNames((previousFileNames) => previousFileNames.filter((fileName) => fileName.file !== file));
   };
 
   return (
     <div>
       <FileUploader
-        onChange={onChangeHandler}
+        onChange={uploadFiles}
         multiple={multiple}
         title={title}
         formatLabel={formatLabel}
         sizeLabel={sizeLabel}
+        accept={['image/png', 'application/pdf', 'image/jpeg']}
         sampleFileLink={
           <Link
             href="http://www.adobe.com/content/dam/Adobe/en/accessibility/pdfs/accessing-pdf-sr.pdf"
@@ -40,32 +107,100 @@ export const all = () => {
           </Link>
         }
       />
-      <FileUploaderList fileList={fileNames} className="mt-4" />
+      <FileUploaderList onDelete={deleteUploadedFile} onRetry={retryUploadFile} fileList={fileNames} className="mt-4" />
     </div>
   );
 };
 
 const customCode = `() => {
   const [fileNames, setFileNames] = React.useState([]);
+  const [errorOccured, setErrorOccured] = React.useState(false);
 
-  const onChangeHandler = (fileList) => {
-    const files = fileList.map(file => {
-      return {
-        file,
-        status: 'completed',
+  const randomBetween = (value) => Math.round(Math.random() * value);
+
+  const imitateUploading = (fileItem) => {
+    const randomProgressChunks = [5, 8, 13];
+    const randomChunkIndex = randomBetween(randomProgressChunks.length - 1);
+    const progressChunk = randomProgressChunks[randomChunkIndex];
+    let baseProgress = fileItem.progress || 0;
+
+    const increment = () => {
+      baseProgress += progressChunk;
+      setProgressFileName(fileItem, baseProgress);
+
+      if (baseProgress < 100) {
+        setTimeout(() => increment(), 100);
       }
-    });
+    };
 
-    setFileNames(files);
+    increment();
+  };
+
+  const setProgressFileName = (fileName, progress = 0) => {
+    setFileNames((previousFileNames) => {
+      const fileNameIndex = previousFileNames.findIndex((fn) => fn.file === fileName.file);
+
+      if (fileNameIndex === -1) {
+        return [...previousFileNames, fileName];
+      }
+
+      return previousFileNames.map((previousFileName, index) => {
+        if (fileNameIndex !== index) return previousFileName;
+
+        return {
+          ...previousFileName,
+          status: progress >= 100 ? 'completed' : 'uploading',
+          progress,
+        };
+      });
+    });
+  };
+
+  const uploadFiles = (fileList) => {
+    if ((!errorOccured && (fileList.length > 1 || fileNames.length > 0))) {
+      const randomErrorIndex = randomBetween(fileList.length - 1);
+      setFileNames((previousFileNames) => [
+        ...previousFileNames,
+        { file: fileList[randomErrorIndex], status: 'error' },
+      ]);
+      setErrorOccured(true);
+
+      if (fileList.length <= 1) {
+        return;
+      }
+
+      fileList.splice(randomErrorIndex, 1);
+    }
+
+    fileList.forEach((file) => {
+      const fileItem = {
+        file,
+        status: 'uploading',
+        progress: 0,
+      };
+
+      imitateUploading(fileItem);
+    });
+  };
+
+  const retryUploadFile = (file) => {
+    uploadFiles([file]);
+  };
+
+  const deleteUploadedFile = (file) => {
+    setFileNames((previousFileNames) => previousFileNames.filter(fileName => fileName.file !== file));
   };
 
   return (
     <div>
       <FileUploader
-        onChange={onChangeHandler}
-        multiple={true}
-        formatLabel='Accepted formats: PDF, jpg'
-        sampleFileLink={(
+        onChange={uploadFiles}
+        multiple
+        title="Upload files"
+        accept={['image/png', 'application/pdf', 'image/jpeg']}
+        formatLabel="Accepted formats: pdf, jpg, jpeg, png"
+        sizeLabel="Maximum size: 25 MB"
+        sampleFileLink={
           <Link
             href="http://www.adobe.com/content/dam/Adobe/en/accessibility/pdfs/accessing-pdf-sr.pdf"
             download="Test.pdf"
@@ -73,9 +208,9 @@ const customCode = `() => {
           >
             Download sample file
           </Link>
-        )}
+        }
       />
-      <FileUploaderList fileList={fileNames} className="mt-4" />
+      <FileUploaderList onDelete={deleteUploadedFile} onRetry={retryUploadFile} fileList={fileNames} className="mt-4" />
     </div>
   );
 }`;
