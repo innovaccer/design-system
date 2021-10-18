16fe6be61419f3b05d54b0ad44aa1da5a369a734
veekays <vikaskumarsingh9838@gmail.com>
2021-10-18 11:19:34 +0530

    docs(message): adds story to message component

---
+++ core/components/atoms/message/__stories__/CustomDescription.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { Message, Row, Column, Text } from '@/index';
+
+export const CustomDescription = () => (
+  <Row>
+    <Column size="4">
+      <Message
+        actions={
+          <>
+            <Text className="cursor-pointer" appearance="link">
+              Try again
+            </Text>
+          </>
+        }
+      >
+        <Text appearance="default">Sorry we couldn't subscribe you. Please try again.</Text>
+      </Message>
+    </Column>
+  </Row>
+);
+
+export default {
+  title: 'Components/Message/Custom Description',
+  component: Message,
+};
