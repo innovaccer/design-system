ead16746e3cec3475f9c0adbaaa0a960988f914c
veekays <vikaskumarsingh9838@gmail.com>
2021-11-25 17:07:21 +0530

    docs(input): update the story to make the input interactive

--- core/components/atoms/input/__stories__/LabelPosition.story.tsx
+++ core/components/atoms/input/__stories__/LabelPosition.story.tsx
@@ -20,6 +20,43 @@ export const labelPosition = () => {
   );
 };
 
+const customCode = `() => {
+  const [inputValue, setInputValue] = React.useState('Joy Lawson');
+  const [secondInputValue, setSecondInputValue] = React.useState('Joy Lawson');
+  return (
+    <div className="d-flex align-items-end">
+      <div>
+        <Label htmlFor="fullName" withInput={true}>
+          Full Name
+        </Label>
+        <Input
+          placeholder="Full name"
+          id="fullName"
+          value={inputValue}
+          onChange={(ev) => {
+            ev.persist();
+            setInputValue(ev.target.value);
+          }}
+        />
+      </div>
+      <div className="d-flex align-items-center ml-9">
+        <Label htmlFor="fullName2" className="mr-6">
+          Full Name
+        </Label>
+        <Input
+          placeholder="Full name"
+          id="fullName2"
+          value={secondInputValue}
+          onChange={(ev) => {
+            ev.persist();
+            setSecondInputValue(ev.target.value);
+          }}
+        />
+      </div>
+    </div>
+  );
+};`;
+
 export default {
   title: 'Components/Input/Label Position',
   component: Input,
@@ -27,6 +64,7 @@ export default {
     docs: {
       docPage: {
         title: 'Input',
+        customCode,
         props: {
           exclude: ['autocomplete'],
         },
