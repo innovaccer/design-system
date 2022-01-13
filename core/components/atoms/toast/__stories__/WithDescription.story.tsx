1cac895d8f32eee1f49da64c3cc67cd35deee52d
nishant1120 <nishant.mishra1120@gmail.com>
2022-01-13 05:46:41 +0530

    fix(toast): add message to  discription story

--- core/components/atoms/toast/__stories__/WithDescription.story.tsx
+++ core/components/atoms/toast/__stories__/WithDescription.story.tsx
@@ -2,7 +2,9 @@ import * as React from 'react';
 import { Toast } from '@/index';
 
 // CSF format story
-export const toastWithDescription = () => <Toast appearance="success" title="Message sent successfully" />;
+export const toastWithDescription = () => (
+  <Toast appearance="success" title="Message sent successfully" message="Description goes here" />
+);
 
 export default {
   title: 'Components/Toast/Toast With Description',
