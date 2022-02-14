4bee0cfb2662bc7a5b729edab337d391d9525551
Anuradha aggarwal <anuradha.mcs18.du@gmail.com>
2022-02-14 19:15:14 +0530

    fix(docs): fix leftnav highlight on tab changes

--- docs/src/components/LeftNav/LeftNav.js
+++ docs/src/components/LeftNav/LeftNav.js
@@ -17,17 +17,16 @@ const LeftNav = (props) => {
   const [active, setActive] = React.useState();
 
   function getActiveNavItem() {
-    if (isBrowser && window.location.pathname && frontmatter.tabs) {
-
-      const url = window.location.pathname.split('/');
+    const pathName = window.location.pathname;
+    if (isBrowser && pathName && frontmatter.tabs) {
+      const url = pathName.split('/');
+      const componentName = pathName.includes('mobile') ? url[2] + '/' + url[3] : url[1] + '/' + url[2];
       const activeMenu = navItems.filter(({ link }) => {
-        return link && link.includes(url[1] + '/' + url[2]);
+        return link && link.includes(componentName);
       });
       return activeMenu[0]?.link;
-
-    } else {
-      return window.location.pathname;
     }
+    return pathName;
   }
 
   useEffect(() => {
