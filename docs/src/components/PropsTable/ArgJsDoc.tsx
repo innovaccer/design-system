deb7f4e26b54e12cb20f5e79036ee63964851d0e
veekays <vikaskumarsingh9838@gmail.com>
2022-03-28 16:20:20 +0530

    fix(docs): update storybook.json in docs data

--- docs/src/components/PropsTable/ArgJsDoc.tsx
+++ docs/src/components/PropsTable/ArgJsDoc.tsx
@@ -57,7 +57,7 @@ export const Table = styled.table(({ theme }) => ({
     '& code': {
       margin: 0,
       display: 'inline-block',
-      fontSize: theme.typography.size.s1,
+      fontSize: theme?.typography?.size.s1,
     },
   },
 }));
