aa3707b04a5d2edb14d6ab23255b9857eaad5974
Satyam Yadav <satyamyadav3@gmail.com>
2022-04-08 18:17:19 +0530

    chore: update release config

--- scripts/version.sh
+++ scripts/version.sh
@@ -1,9 +1,9 @@
-LOG=$(git log $(git describe --tags --abbrev=0 @^)..@ --pretty=format:"%h %s" --no-merges | sed -e 's/^/* /')
-PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
-DATE=$(date +%F)
+# LOG=$(git log $(git describe --tags --abbrev=0 @^)..@ --pretty=format:"%h %s" --no-merges | sed -e 's/^/* /')
+# PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+# DATE=$(date +%F)
 
 # Append changelog
-echo "\n-------------------\n **v$PACKAGE_VERSION**  ($DATE) \n\n$LOG";
+# echo "\n-------------------\n **v$PACKAGE_VERSION**  ($DATE) \n\n$LOG";
 
 # build
 npm run build && 
