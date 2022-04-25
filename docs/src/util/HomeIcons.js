dd915f1d3dfaf585326702fb740dae29833fb5e3
Anuradha Aggarwal <I1333@IT002600.local>
2022-04-25 18:28:38 +0530

    fix(docs): replace icons with svg on home page

---
+++ docs/src/util/HomeIcons.js
@@ -0,0 +1,70 @@
+import React from "react";
+
+export const Components = () => <svg width="48px" height="48px" viewBox="0 0 48 48" version="1.1" xmlns="http://www.w3.org/2000/svg">
+  <title>Group 15</title>
+  <g id="Web" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+    <g id="Landing-screen---Top-navigation" transform="translate(-744.000000, -640.000000)">
+      <g id="Group-15" transform="translate(744.000000, 640.000000)">
+        <g id="Group" fill="#F9E2E2">
+          <circle id="Background" cx="24" cy="24" r="24"></circle>
+        </g>
+        <g id="widgets_black_24dp" transform="translate(12.000000, 12.000000)">
+          <polygon id="Path" points="0 0 24 0 24 24 0 24"></polygon>
+          <path d="M13,14 L13,20 C13,20.55 13.45,21 14,21 L20,21 C20.55,21 21,20.55 21,20 L21,14 C21,13.45 20.55,13 20,13 L14,13 C13.45,13 13,13.45 13,14 Z M4,21 L10,21 C10.55,21 11,20.55 11,20 L11,14 C11,13.45 10.55,13 10,13 L4,13 C3.45,13 3,13.45 3,14 L3,20 C3,20.55 3.45,21 4,21 Z M3,4 L3,10 C3,10.55 3.45,11 4,11 L10,11 C10.55,11 11,10.55 11,10 L11,4 C11,3.45 10.55,3 10,3 L4,3 C3.45,3 3,3.45 3,4 Z M15.95,2.4 L11.7,6.64 C11.31,7.03 11.31,7.66 11.7,8.05 L15.95,12.3 C16.34,12.69 16.97,12.69 17.36,12.3 L21.61,8.05 C22,7.66 22,7.03 21.61,6.64 L17.37,2.4 C16.98,2.01 16.34,2.01 15.95,2.4 Z" id="Shape" fill="#D93737" fillRule="nonzero"></path>
+        </g>
+      </g>
+    </g>
+  </g>
+</svg>
+
+export const Foundations = () => <svg width="48px" height="48px" viewBox="0 0 48 48" version="1.1" xmlns="http://www.w3.org/2000/svg">
+  <title>Group 13</title>
+  <g id="Web" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+    <g id="Landing-screen---Top-navigation" transform="translate(-150.000000, -640.000000)">
+      <g id="Group-13" transform="translate(150.000000, 640.000000)">
+        <circle id="Background" fill="#D7EFDF" cx="24" cy="24" r="24"></circle>
+        <g id="apartment_black_24dp" transform="translate(12.000000, 12.000000)">
+          <g id="Group">
+            <rect id="Rectangle" x="0" y="0" width="24" height="24"></rect>
+            <rect id="Rectangle" x="0" y="0" width="24" height="24"></rect>
+          </g>
+          <path d="M17,11 L17,5 C17,3.9 16.1,3 15,3 L9,3 C7.9,3 7,3.9 7,5 L7,7 L5,7 C3.9,7 3,7.9 3,9 L3,19 C3,20.1 3.9,21 5,21 L10,21 C10.55,21 11,20.55 11,20 L11,17 L13,17 L13,20 C13,20.55 13.45,21 14,21 L19,21 C20.1,21 21,20.1 21,19 L21,13 C21,11.9 20.1,11 19,11 L17,11 Z M7,19 L5,19 L5,17 L7,17 L7,19 Z M7,15 L5,15 L5,13 L7,13 L7,15 Z M7,11 L5,11 L5,9 L7,9 L7,11 Z M11,15 L9,15 L9,13 L11,13 L11,15 Z M11,11 L9,11 L9,9 L11,9 L11,11 Z M11,7 L9,7 L9,5 L11,5 L11,7 Z M15,15 L13,15 L13,13 L15,13 L15,15 Z M15,11 L13,11 L13,9 L15,9 L15,11 Z M15,7 L13,7 L13,5 L15,5 L15,7 Z M19,19 L17,19 L17,17 L19,17 L19,19 Z M19,15 L17,15 L17,13 L19,13 L19,15 Z" id="Shape" fill="#2EA843" fillRule="nonzero"></path>
+        </g>
+      </g>
+    </g>
+  </g>
+</svg>
+
+export const Content = () => <svg width="48px" height="48px" viewBox="0 0 48 48" version="1.1" xmlns="http://www.w3.org/2000/svg">
+  <title>Group 14</title>
+  <g id="Web" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+    <g id="Landing-screen---Top-navigation" transform="translate(-447.000000, -640.000000)">
+      <g id="Group-14" transform="translate(447.000000, 640.000000)">
+        <g id="Group" fill="#FDE6CE">
+          <circle id="Background" cx="24" cy="24" r="24"></circle>
+        </g>
+        <g id="notes_black_24dp" transform="translate(12.000000, 12.000000)">
+          <polygon id="Path" points="0 0 24 0 24 24 0 24"></polygon>
+          <path d="M20,11 L4,11 C3.45,11 3,11.45 3,12 C3,12.55 3.45,13 4,13 L20,13 C20.55,13 21,12.55 21,12 C21,11.45 20.55,11 20,11 Z M4,18 L14,18 C14.55,18 15,17.55 15,17 C15,16.45 14.55,16 14,16 L4,16 C3.45,16 3,16.45 3,17 C3,17.55 3.45,18 4,18 Z M20,6 L4,6 C3.45,6 3,6.45 3,7 L3,7.01 C3,7.56 3.45,8.01 4,8.01 L20,8.01 C20.55,8.01 21,7.56 21,7.01 L21,7 C21,6.45 20.55,6 20,6 Z" id="Shape" fill="#F07D00" fillRule="nonzero"></path>
+        </g>
+      </g>
+    </g>
+  </g>
+</svg>
+
+export const Patterns = () => <svg width="48px" height="48px" viewBox="0 0 48 48" version="1.1" xmlns="http://www.w3.org/2000/svg">
+  <title>Group 16</title>
+  <g id="Web" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+    <g id="Landing-screen---Top-navigation" transform="translate(-1041.000000, -640.000000)">
+      <g id="Group-16" transform="translate(1041.000000, 640.000000)">
+        <g id="Group" fill="#E0D8EE">
+          <circle id="Background" cx="24" cy="24" r="24"></circle>
+        </g>
+        <g id="pattern_black_24dp" transform="translate(12.000000, 12.000000)">
+          <polygon id="Path" points="0 0 24 0 24 24 0 24"></polygon>
+          <path d="M4,6 C4,4.9 4.9,4 6,4 C7.1,4 8,4.9 8,6 C8,7.1 7.1,8 6,8 C4.9,8 4,7.1 4,6 Z M6,14 C7.1,14 8,13.1 8,12 C8,10.9 7.1,10 6,10 C4.9,10 4,10.9 4,12 C4,13.1 4.9,14 6,14 Z M12,8 C13.1,8 14,7.1 14,6 C14,4.9 13.1,4 12,4 C10.9,4 10,4.9 10,6 C10,7.1 10.9,8 12,8 Z M18,10 C16.9,10 16,10.9 16,12 C16,13.1 16.9,14 18,14 C19.1,14 20,13.1 20,12 C20,10.9 19.1,10 18,10 Z M17.98,16 C17.24,16 16.61,16.4 16.26,17 L13.72,17 C13.38,16.4 12.74,16 12,16 C11.26,16 10.63,16.4 10.28,17 L8.41,17 L11.48,13.93 C11.65,13.97 11.82,14 12,14 C13.1,14 14,13.1 14,12 C14,11.82 13.97,11.65 13.93,11.49 L17.49,7.93 C17.65,7.97 17.82,8 18,8 C19.1,8 20,7.1 20,6 C20,4.9 19.1,4 18,4 C16.9,4 16,4.9 16,6 C16,6.18 16.03,6.35 16.07,6.51 L12.51,10.07 C12.35,10.03 12.18,10 12,10 C10.9,10 10,10.9 10,12 C10,12.18 10.03,12.35 10.07,12.51 L6.51,16.07 C6.35,16.03 6.18,16 6,16 C4.9,16 4,16.9 4,18 C4,19.1 4.9,20 6,20 C6.74,20 7.37,19.6 7.72,19 L10.29,19 C10.63,19.6 11.27,20 12.01,20 C12.75,20 13.38,19.6 13.73,19 L16.28,19 C16.62,19.6 17.26,20 18,20 C19.1,20 20,19.1 20,18 C19.98,16.9 19.08,16 17.98,16 Z" id="Shape" fill="#7A53B2" fillRule="nonzero"></path>
+        </g>
+      </g>
+    </g>
+  </g>
+</svg>
