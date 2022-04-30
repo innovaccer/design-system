19a42e3c6d955f86ce39cf91ed7cb9a3114e4f32
Anuradha Aggarwal <I1333@IT002600.local>
2022-04-30 14:03:25 +0530

    feat(docs): add status tag on components overview page

--- docs/src/data/components/Overview.js
+++ docs/src/data/components/Overview.js
@@ -1,14 +1,26 @@
 import React, { useState } from 'react';
-import { Card, CardBody, Row, Column, Heading, Input, EmptyState, Button } from '@innovaccer/design-system';
+import { Card, CardBody, Row, Column, Heading, Input, EmptyState, Badge } from '@innovaccer/design-system';
 import { Link } from 'gatsby';
 import './overview.css';
 import { debounce } from '../../util/Helpers';
 import noResultImage from './images/noresult.png';
 
+const StatusTag = ({ category }) => {
+
+  const categoryName = category?.toUpperCase();
+
+  const appearance = {
+    "CODE UNAVAILABLE": "accent1",
+    "BETA": "accent2"
+  }
+
+  return <Badge appearance={appearance[categoryName]}>{category}</Badge>
+}
+
 function Overview({ data, mode }) {
 
   const [previewList, setPreviewList] = useState(data);
-  const [searchKey , setSearchKey] = useState('')
+  const [searchKey, setSearchKey] = useState('')
 
   const onSearchHandler = debounce((target) => {
     setSearchKey(target.value)
@@ -30,7 +42,7 @@ function Overview({ data, mode }) {
       <Row>
         {
           previewList.length > 0 ?
-            previewList.map(({ image = () => (<img alt='' />), name, link = '' }) => {
+            previewList.map(({ image = () => (<img alt='' />), name, status, link = '' }) => {
               return (
                 <Column
                   size={4}
@@ -38,32 +50,40 @@ function Overview({ data, mode }) {
                   className="pr-6 pb-6"
                 >
 
-                  <Link className='card-link' disabled={!link.length} to={ mode === "mobile" ? `/mobile/components/${(link)}` : `/components/${(link)}`}>
+                  <Link
+                    className='card-link'
+                    disabled={!link.length}
+                    to={mode === "mobile" ? `/mobile/components/${(link)}` : `/components/${(link)}`}
+                  >
                     <Card
                       shadow='none'
-                      className='w-100 overflow-hidden overview-card pb-5'
+                      className='w-100 overflow-hidden overview-card pb-5 h-100'
                     >
                       <CardBody className='px-0'>
-                        <div className='py-5 card-opacity'>
+                        <div className='py-5 card-opacity d-flex flex-column'>
+                          <div className='d-flex justify-content-end mr-5'>
+                            {status && <StatusTag category={status} />}
+                          </div>
+
                           <div className='d-flex justify-content-center align-items-center' style={{ overflow: 'hidden', height: '136px' }}>
                             {React.createElement(image)}
                           </div>
                         </div>
-                        <Heading size="s" className='ml-6'>
+                        <Heading size="s" className='ml-6 position-absolute card-heading'>
                           {name}
                         </Heading>
                       </CardBody>
                     </Card>
                   </Link>
-
                 </Column>
               )
             }) :
             <div className="d-flex justify-content-center" style={{ transform: "translate(50%,64px)" }}>
-              <EmptyState 
+              <EmptyState
+                size="small"
                 description="Sorry! We could not find any match. Please try again"
                 imageSrc={noResultImage}
-                size="small" title={`No results found for ${"'"+searchKey+"'"}`}
+                title={`No results found for '${searchKey}'`}
               />
             </div>
         }
