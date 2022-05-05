49cd3a8b5255d60efef1c89ad49ab4f0965b541d
Anuradha Aggarwal <I1333@IT002600.local>
2022-05-05 17:01:12 +0530

    fix(docs): fix heading tooltip alignment issue in mdx

--- docs/src/components/MDXHeading.js
+++ docs/src/components/MDXHeading.js
@@ -2,60 +2,60 @@ import React, { useState } from "react";
 import { Heading, Icon, Tooltip } from "@innovaccer/design-system";
 import { copyMessage, copyMessageSuccess } from "../util/constants.js";
 
+const copyToClipboard = (str) => {
+  navigator.clipboard.writeText(str);
+};
+
 const MDXHeading = ({ size, headingInfo }) => {
   const refHeading = React.createRef();
   const [isHovered, setIsHovered] = useState(false);
   const [tooltipName, setTooltipName] = useState(copyMessage);
-  const [isTooltipActiveHeading, setTooltipActiveHeading] = useState(false);
-  const hoverHandler = (type) => {
-    if (type === "Enter") {
-      setIsHovered(true);
-    } else {
-      setIsHovered(false);
-      setTooltipName(copyMessage);
-      setTooltipActiveHeading(false);
-    }
+  const [isTooltipActive, setTooltipActive] = useState(false);
+
+  const onMouseLeaveHandler = () => {
+    setIsHovered(false);
+    setTooltipName(copyMessage);
+    setTooltipActive(false);
   };
-  const copyToClipboard = (str) => {
-    navigator.clipboard.writeText(str);
-  };
-  const hoverCopyHandler = (id) => {
+
+  const onClickHandler = (id) => {
     const currURL = window.location.href;
+
     if (currURL.includes(id)) {
       copyToClipboard(currURL);
-    } else if (currURL.includes("#")) {
+    }
+    else if (currURL.includes("#")) {
       copyToClipboard(currURL.slice(0, currURL.indexOf("#") + 1) + id);
-    } else {
+    }
+    else {
       copyToClipboard(currURL + "#" + id);
     }
-    setTooltipActiveHeading(true);
+    setTooltipActive(true);
     setTooltipName(copyMessageSuccess);
   };
+
+  const sizeMarginMapper = {
+    s: "mt-4",
+    m: "mt-4",
+    l: "mt-6"
+  }
+
   return (
     <div
-      onMouseEnter={() => hoverHandler("Enter", headingInfo)}
-      onMouseLeave={() => hoverHandler("Leave", headingInfo)}
-      className={`d-inline-flex w-100 ${size === "s" ? "align-items-center" : "align-items-baseline"
-        }`}
       ref={refHeading}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={onMouseLeaveHandler}
+      className={`d-inline-flex w-100 align-items-center ${sizeMarginMapper[size]}`}
     >
       <Heading
-        className={`mr-4 
-          ${size === "s" || size === "m" ?
-            "mt-4"
-            : size === "l" ?
-              "mt-6"
-              : ""
-          }`
-        }
-
+        className="mr-4"
         size={size}
         {...headingInfo}
       />
-      <div className="cursor-pointer d-inline-block">
+      <div className="cursor-pointer d-flex">
         {isHovered && (
           <Tooltip
-            open={isTooltipActiveHeading}
+            open={isTooltipActive}
             tooltip={tooltipName}
             position="bottom"
             appendToBody={false}
@@ -63,9 +63,7 @@ const MDXHeading = ({ size, headingInfo }) => {
             className="Heading-tooltip"
           >
             <Icon
-              onClick={() => {
-                hoverCopyHandler(headingInfo.id);
-              }}
+              onClick={() => onClickHandler(headingInfo.id)}
               appearance="subtle"
               type="filled"
               size={16}
@@ -77,4 +75,5 @@ const MDXHeading = ({ size, headingInfo }) => {
     </div>
   );
 };
+
 export default MDXHeading;
