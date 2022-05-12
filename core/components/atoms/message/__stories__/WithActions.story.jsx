367701ca94c4e4ecd0e08948d275bd229f8c9f69
Anuradha Aggarwal <I1333@IT002600.local>
2022-05-12 20:55:06 +0530

    fix(message): fix stories width for message component

--- core/components/atoms/message/__stories__/WithActions.story.jsx
+++ core/components/atoms/message/__stories__/WithActions.story.jsx
@@ -4,7 +4,7 @@ import { Message, Row, Column, Text } from '@/index';
 // CSF format story
 export const MessageWithActions = () => (
   <Row>
-    <Column size="4">
+    <Column size="8">
       <Message
         appearance="alert"
         description="Sorry we couldn't subscribe you. Please try again."
