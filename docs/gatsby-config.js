4b3d6989e714629511e6aa988b5bfd1078663021
Anuradha Aggarwal <I1333@IT002600.local>
2022-05-17 01:34:48 +0530

    feat(docs): add google analytics support

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -186,6 +186,25 @@ module.exports = {
         protocol: "https",
         hostname: "design-dev.innovaccer.com",
       },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a page view event will be fired for all of them.
+        trackingIds: [
+          process.env.GA_TRACKING_ID, // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        }
+      }
     }
   ]
 }
