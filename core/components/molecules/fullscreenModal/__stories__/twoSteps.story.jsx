ebec635ea6370b5472d2fcee8c25d39b31dbd5a1
Anuradha Aggarwal <I1333@IT002600.local>
2022-05-18 09:27:26 +0530

    fix(stories): fix full screen modal stories

--- core/components/molecules/fullscreenModal/__stories__/twoSteps.story.jsx
+++ core/components/molecules/fullscreenModal/__stories__/twoSteps.story.jsx
@@ -16,19 +16,9 @@ export const twoSteps = () => {
 
   return (
     <div>
-      <Paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua.
-        <br />
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        <br />
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <br />
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        <br />
-      </Paragraph>
 
       <Button appearance="primary" onClick={openModal}>
-        Open modal
+        Open Modal
       </Button>
 
       <FullscreenModal
@@ -101,19 +91,9 @@ const customCode = `
 
   return (
     <div>
-      <Paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua.
-        <br />
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        <br />
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <br />
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        <br />
-      </Paragraph>
 
       <Button className="m-8" appearance="primary" onClick={openModal}>
-        Open modal
+        Open Modal
       </Button>
 
       <FullscreenModal
