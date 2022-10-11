fa3e9128cc027520294208152dbad2b6ca6182af
Varnika <varnika.jain@innovaccer.com>
2022-10-11 11:06:54 +0530

    test(docs): add cypress test for foundation mobile page

---
+++ docs/cypress/e2e/foundationMobile.cy.js
@@ -0,0 +1,38 @@
+const hostURL = Cypress.env('HOST_URL') || 'http://localhost:8000';
+const foundationURL = hostURL + '/mobile/foundations/layout/';
+
+describe('Cypress Test of foundations mobile page', () => {
+  const navLink = [];
+  before(() => {
+    cy.visit(foundationURL);
+    cy.leftnavTraverse(navLink);
+  });
+
+  it('check for all the links it redirects', () => {
+    navLink.forEach((page) => {
+      cy.visit(page);
+      cy.linkVisit();
+    });
+  });
+
+  it('check if all links of table of contents direct to the content', () => {
+    navLink.forEach((page) => {
+      cy.visit(page);
+      cy.tableOfContent();
+    });
+  });
+
+  it('check if images of page are loading', () => {
+    navLink.forEach((page) => {
+      cy.visit(page);
+      cy.imageRender();
+    });
+  });
+
+  it('check for web and mobile tile of foundation page', () => {
+    navLink.forEach((page) => {
+      cy.visit(page);
+      cy.tileToggle();
+    });
+  });
+});
