ae6186874534e798bedc7968eede813418a6d784
Varnika <varnika.jain@innovaccer.com>
2022-10-25 19:50:22 +0530

    fix(radio): state related issues

--- core/components/atoms/radio/__stories__/Alignment.story.jsx
+++ core/components/atoms/radio/__stories__/Alignment.story.jsx
@@ -4,6 +4,9 @@ import { Radio, Row, Column, Text } from '@/index';
 export const AlignmentOfRadioGroup = () => (
   <Row>
     <Column>
+      <div className="pb-5">
+        <Text>Horizontal alignment</Text>
+      </div>
       <div className="d-flex">
         <div className="mr-9 ">
           <Radio size={'regular'} label={'Male'} name={'Gender'} value={'Male'} defaultChecked={true} />
@@ -17,21 +20,17 @@ export const AlignmentOfRadioGroup = () => (
           <Radio size={'regular'} label={'Other'} name={'Gender'} value={'Other'} />
         </div>
       </div>
-
-      <div className="pt-5">
-        <Text>Horizontal alignment</Text>
-      </div>
     </Column>
     <Column>
+      <div className="pb-5">
+        <Text>Vertical alignment</Text>
+      </div>
+
       <Radio size={'regular'} label={'Male'} name={'gender'} value={'Male'} defaultChecked={true} />
 
       <Radio size={'regular'} label={'Female'} name={'gender'} value={'Female'} />
 
       <Radio size={'regular'} label={'Other'} name={'gender'} value={'Other'} />
-
-      <div className="pt-5">
-        <Text>Vertical alignment</Text>
-      </div>
     </Column>
   </Row>
 );
