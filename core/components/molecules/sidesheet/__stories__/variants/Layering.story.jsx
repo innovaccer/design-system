6b1a1138a398df0922ffb8a1ea9540133f079e5e
AmanVerma <aman.verma@innovaccer.com>
2022-12-13 15:31:47 +0530

    fix(sidesheet): fix layering of nested components

--- core/components/molecules/sidesheet/__stories__/variants/Layering.story.jsx
+++ core/components/molecules/sidesheet/__stories__/variants/Layering.story.jsx
@@ -8,7 +8,7 @@ export const layering = () => {
   const seperator = false;
   const backIcon = false;
   const stickFooter = false;
-  const backdropClose = false;
+  const backdropClose = true;
   const dimension = 'regular';
 
   const onClose = () => {
@@ -101,64 +101,91 @@ export const layering = () => {
 const customCode = `() => {
   const [open, setOpen] = React.useState(false);
   const [openSecond, setOpenSecond] = React.useState(false);
+  const seperator = false;
+  const backIcon = false;
+  const stickFooter = false;
+  const backdropClose = true;
+  const dimension = 'regular';
 
   const onClose = () => {
-    setOpen(!open);
+    setOpen(false);
+    action('on close triggered')();
   };
 
-  const onSecondOverlayClose = () => {
-    setOpenSecond(!openSecond);
-  }
+  const onCloseSecond = () => {
+    setOpenSecond(false);
+    action('on close triggered')();
+  };
+
+  const backIconCallback = () => {
+    action('back icon clicked')();
+  };
 
   const headerOptions = {
+    backIcon,
+    backIconCallback: backIcon ? backIconCallback : undefined,
     heading: 'Heading',
-    subHeading: 'Subheading'
+    subHeading: 'Subheading',
   };
 
   const options = {
     onClose,
     open,
+    dimension,
+    seperator,
     headerOptions,
+    stickFooter,
+    backdropClose,
     footer: (
       <>
-        <Button appearance="primary" className="mr-4" onClick={() => setOpenSecond(true)}>Open</Button>
+        <Button appearance="primary" className="mr-4" onClick={() => setOpenSecond(true)}>
+          Open
+        </Button>
         <Button appearance="basic">Basic</Button>
       </>
-    )
+    ),
   };
 
-  const secondOverlayOptions = {
-    onClose: onSecondOverlayClose,
-    open: openSecond,
+  const SecondOverlayOptions = {
+    dimension,
+    seperator,
     headerOptions,
+    stickFooter,
+    backdropClose,
+    onClose: onCloseSecond,
+    open: openSecond,
     footer: (
       <>
-        <Button appearance="primary" className="mr-4">Primary</Button>
-        <Button appearance="basic">Basic</Button>
+        <Button appearance="primary" className="mr-4">
+          Primary
+        </Button>
       </>
-    )
+    ),
   };
 
   const modalDescriptionOptions = {
     title: 'Description Title',
     description: 'Adding a subheading clearly indicates the hierarchy of the information.',
-    removePadding: true
+    removePadding: true,
   };
 
   const modalDescriptionOptionsWithoutTitle = {
     description: 'Card Sections include supporting text like an article summary or a restaurant description.',
-    removePadding: true
+    removePadding: true,
   };
 
   return (
     <div>
-      <Button appearance="primary" onClick={() => setOpen(true)}>Open Sidesheet</Button>
+      <Button appearance="primary" onClick={() => setOpen(true)}>
+        Open Sidesheet
+      </Button>
       <Sidesheet {...options} dimension="large" closeOnEscape={true}>
         <Text>Modal Body</Text>
         <ModalDescription {...modalDescriptionOptions} />
         <ModalDescription {...modalDescriptionOptionsWithoutTitle} />
       </Sidesheet>
-      <Sidesheet {...secondOverlayOptions} closeOnEscape={true}>
+
+      <Sidesheet {...SecondOverlayOptions} closeOnEscape={true}>
         <Text>Modal Body Part Two</Text>
         <ModalDescription {...modalDescriptionOptions} />
         <ModalDescription {...modalDescriptionOptionsWithoutTitle} />
