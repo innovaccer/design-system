69d910c0021bd80ebab1b16fed14e8f43fd31c83
veekays <vikaskumarsingh9838@gmail.com>
2022-12-19 13:43:22 +0530

    fix(popover): update the story of popover component

--- core/components/molecules/popover/__stories__/variants/Position.story.jsx
+++ core/components/molecules/popover/__stories__/variants/Position.story.jsx
@@ -4,18 +4,13 @@ import { Text, Button, Popover } from '@/index';
 // CSF format story
 export const position = () => {
   const positions = ['top', 'top-start', 'top-end', 'bottom', 'bottom-start', 'bottom-end', 'left', 'right'];
-  const style = {
-    display: 'flex',
-    'flex-wrap': 'wrap',
-  };
-
   const getTrigger = (pos) => <Button appearance="basic">{pos}</Button>;
 
   return (
-    <div style={style}>
+    <div className="d-flex flex-wrap">
       {positions.map((pos, ind) => {
         return (
-          <div key={ind} className={ind < 3 ? 'mt-11 mr-13' : 'mt-5 mb-11 mr-13'}>
+          <div key={ind} className={`mr-13 ml-12 ${ind < 3 ? 'mt-11' : 'mt-5 mb-11'}`}>
             <Popover trigger={getTrigger(pos)} position={pos} open={true}>
               <div style={{ width: 100 }} className="mx-6 my-6">
                 <Text>Popup</Text>
