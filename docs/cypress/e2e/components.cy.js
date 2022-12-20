6aa2053115699d3492969d225d6ea62b90a2ff5a
Varnika <varnika.jain@innovaccer.com>
2022-12-20 16:54:37 +0530

    test(docs): add cypress test for components page

---
+++ docs/cypress/e2e/components.cy.js
@@ -0,0 +1,163 @@
+const hostURL = Cypress.env('HOST_URL') || 'http://localhost:8000';
+const componentURL = hostURL + '/components/overview/all-components/';
+const statusURL = hostURL + '/components/overview/status/';
+
+describe('Cypress Test of component page', () => {
+  const navLink = [];
+  before(() => {
+    cy.visit(componentURL);
+    cy.leftnavTraverse(navLink);
+  });
+
+  it('check for the overview page', () => {
+    cy.visit(componentURL);
+    cy.searchBar('abc', 'Buttons');
+    cy.linkVisit();
+    cy.tileToggle();
+  });
+
+  it('check navigation of status table', () => {
+    cy.visit(statusURL);
+    cy.statusTable();
+  });
+
+  it('check for all the links it redirects', () => {
+    navLink.slice(1, 10).forEach((page) => {
+      if (!page.includes('breadcrumbs')) {
+        cy.visit(page);
+        cy.linkVisit();
+      }
+    });
+  });
+
+  it('check if all links of table of contents direct to the content', () => {
+    navLink.slice(1, 10).forEach((page) => {
+      cy.visit(page);
+      cy.tableOfContent();
+    });
+  });
+
+  it('check if live preview is loading', () => {
+    navLink.slice(1, 10).forEach((page) => {
+      cy.visit(page);
+      cy.livePreview();
+    });
+  });
+
+  it('check if tabs exsits and they are loading', () => {
+    navLink.slice(1, 10).forEach((page) => {
+      cy.visit(page);
+      cy.tabsVisit();
+    });
+  });
+});
+
+describe('Cypress Test of component page', () => {
+  const navLink = [];
+  before(() => {
+    cy.visit(componentURL);
+    cy.leftnavTraverse(navLink);
+  });
+
+  it('check for all the links it redirects', () => {
+    navLink.slice(10, 20).forEach((page) => {
+      cy.visit(page);
+      cy.linkVisit();
+    });
+  });
+
+  it('check if all links of table of contents direct to the content', () => {
+    navLink.slice(10, 20).forEach((page) => {
+      cy.visit(page);
+      cy.tableOfContent();
+    });
+  });
+
+  it('check if live preview is loading', () => {
+    navLink.slice(10, 20).forEach((page) => {
+      cy.visit(page);
+      cy.livePreview();
+    });
+  });
+
+  it('check if tabs exsits and they are loading', () => {
+    navLink.slice(10, 20).forEach((page) => {
+      cy.visit(page);
+      cy.tabsVisit();
+    });
+  });
+});
+
+describe('Cypress Test of component page', () => {
+  const navLink = [];
+  before(() => {
+    cy.visit(componentURL);
+    cy.leftnavTraverse(navLink);
+  });
+
+  it('check for all the links it redirects', () => {
+    navLink.slice(20, 30).forEach((page) => {
+      if (!page.includes('pageHeaders')) {
+        cy.visit(page);
+        cy.linkVisit();
+      }
+    });
+  });
+
+  it('check if all links of table of contents direct to the content', () => {
+    navLink.slice(20, 30).forEach((page) => {
+      cy.visit(page);
+      cy.tableOfContent();
+    });
+  });
+
+  it('check if live preview is loading', () => {
+    navLink.slice(20, 30).forEach((page) => {
+      cy.visit(page);
+      cy.livePreview();
+    });
+  });
+
+  it('check if tabs exsits and they are loading', () => {
+    navLink.slice(20, 30).forEach((page) => {
+      cy.visit(page);
+      cy.tabsVisit();
+    });
+  });
+});
+
+describe('Cypress Test of component page', () => {
+  const navLink = [];
+  before(() => {
+    cy.visit(componentURL);
+    cy.leftnavTraverse(navLink);
+  });
+
+  it('check for all the links it redirects', () => {
+    navLink.slice(30).forEach((page) => {
+      cy.visit(page);
+      cy.linkVisit();
+    });
+  });
+
+  it('check if all links of table of contents direct to the content', () => {
+    navLink.slice(30).forEach((page) => {
+      cy.visit(page);
+      cy.tableOfContent();
+    });
+  });
+
+  it('check if live preview is loading', () => {
+    navLink.slice(30).forEach((page) => {
+      cy.visit(page);
+      cy.livePreview();
+    });
+  });
+
+  it('check if tabs exsits and they are loading', () => {
+    navLink.slice(30).forEach((page) => {
+      cy.visit(page);
+      cy.tabsVisit();
+    });
+  });
+});
