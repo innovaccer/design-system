059ca28426ac4b68cdaf5883a383d096d70e0c8e
Rashi-Gupta-2000 <rashi.gupta@innovaccer.com>
2022-12-27 16:08:09 +0530

    fix(emptyState): replace style tag with classname in stories

--- core/components/molecules/emptyState/__stories__/noContentAvailable.story.jsx
+++ core/components/molecules/emptyState/__stories__/noContentAvailable.story.jsx
@@ -6,7 +6,7 @@ export const noContentAvailable = () => {
   const title = 'Manage your outreach campaigns';
   const description = 'Campaigns let you reach out to patients with text messages, emails and voice calls';
   return (
-    <div className="h-100 pb-5" style={{ backgroundColor: 'var(--secondary-lightest)' }}>
+    <div className="h-100 pb-5 bg-secondary-lightest">
       <EmptyState title={title} description={description} imageSrc={noContent} size="large">
         <Button size="large" appearance="primary">
           Reload page
