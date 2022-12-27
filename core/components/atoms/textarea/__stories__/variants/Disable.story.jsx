0534ba6669588cefab25f4aea13a4052be165d7d
Rashi-Gupta-2000 <rashi.gupta@innovaccer.com>
2022-12-27 15:51:37 +0530

    fix(textarea): replace style tag with classname in stories

--- core/components/atoms/textarea/__stories__/variants/Disable.story.jsx
+++ core/components/atoms/textarea/__stories__/variants/Disable.story.jsx
@@ -1,37 +1,26 @@
 import * as React from 'react';
-import { Text, Textarea } from '@/index';
+import { Label, Textarea, Row, Column } from '@/index';
 import { action } from '@/utils/action';
 
 export const Disable = () => {
-  const innerStyle = {
-    display: 'flex',
-    'flex-direction': 'column',
-    alignItems: 'center',
-    maxWidth: '300px',
-  };
-
   const BooleanValue = [true, false];
 
   return (
-    <div className="Row">
+    <Row>
       {BooleanValue.map((value, ind) => (
-        <div style={innerStyle} className="mr-9 mb-5" key={ind}>
-          <div style={innerStyle} className="mr-9 mb-5">
-            <div className="w-100 mb-6">
-              <Textarea
-                name="Textarea"
-                onChange={action('on-change')}
-                placeholder="Placeholder"
-                disabled={value}
-                rows={3}
-                aria-labelledby="Textarea"
-              />
-            </div>
-            <Text weight="strong">{value ? 'Disabled' : 'Enabled'}</Text>
-          </div>
-        </div>
+        <Column key={ind} className="p-4">
+          <Label withInput={true}>{value ? 'Disabled' : 'Enabled'}</Label>
+          <Textarea
+            name="Textarea"
+            onChange={action('on-change')}
+            placeholder="Placeholder"
+            disabled={value}
+            rows={3}
+            aria-labelledby="Textarea"
+          />
+        </Column>
       ))}
-    </div>
+    </Row>
   );
 };
 
