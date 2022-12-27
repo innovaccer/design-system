148635dd9021da0b57402f162bd5029d60a1dbe8
Rashi-Gupta-2000 <rashi.gupta@innovaccer.com>
2022-12-27 13:42:44 +0530

    fix(message): replace style tag with classname in stories

--- core/components/atoms/message/__stories__/variants/Appearance.story.jsx
+++ core/components/atoms/message/__stories__/variants/Appearance.story.jsx
@@ -1,39 +1,36 @@
 import * as React from 'react';
-import { Message, Text } from '@/index';
+import { Message, Text, Row, Column } from '@/index';
 
 // CSF format story
 export const appearanceWithoutTitle = () => {
   const appearances = ['alert', 'info', 'success', 'warning'];
-  const innerStyle = {
-    display: 'flex',
-    'align-items': 'center',
-    'flex-direction': 'column',
-  };
   return (
-    <div className="Row">
+    <Row>
       {appearances.map((appear, ind) => {
         return (
-          <div key={ind} style={innerStyle} className="mr-7 mb-7 w-25 d-flex">
+          <Column sizeM="4" className="p-4" key={ind}>
             <Message
-              appearance={appear}
-              description="Patient record has been updated with new records."
               actions={
                 <>
-                  <Text className="cursor-pointer" appearance="link">
+                  <Text appearance="link" className="cursor-pointer">
                     Action 1
                   </Text>
-                  <Text className="ml-5 cursor-pointer" appearance="link">
+                  <Text appearance="link" className="ml-5 cursor-pointer">
                     Action 2
                   </Text>
                 </>
               }
+              appearance={appear}
+              description="Patient record has been updated with new records."
             />
             <br />
-            <Text weight="strong">{appear.charAt(0).toUpperCase() + appear.slice(1)}</Text>
-          </div>
+            <Text weight="strong" className="d-flex flex-column align-items-center">
+              {appear.charAt(0).toUpperCase() + appear.slice(1)}
+            </Text>
+          </Column>
         );
       })}
-    </div>
+    </Row>
   );
 };
 
