b3d39694b431a4b5c75120a038bf18341ae1a1f6
Rashi-Gupta-2000 <rashi.gupta@innovaccer.com>
2022-12-31 03:22:40 +0530

    chore(navigation): deprecate navigation component

---
+++ core/components/organisms/navigation/__stories__/index.story.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+// CSF format story
+export const all = () => <></>;
+
+export default {
+  title: 'Components/Navigation/All',
+  parameters: {
+    docs: {
+      docPage: {
+        title: 'Navigation',
+        description: 'Navigation Component has been Deprecated. Please use VerticalNav and HorizontalNav instead.',
+      },
+    },
+  },
+};
