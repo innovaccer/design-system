313216074c350d2c915a1243569653c6a30f4f07
Rashi-Gupta-2000 <rashi.gupta@innovaccer.com>
2023-01-02 18:33:06 +0530

    fix(metricInput): replace style tag with classname in stories

--- core/components/atoms/metricInput/__stories__/DefaultMetric.story.jsx
+++ core/components/atoms/metricInput/__stories__/DefaultMetric.story.jsx
@@ -1,16 +1,16 @@
 import * as React from 'react';
-import { MetricInput, Label } from '@/index';
+import { MetricInput, Label, Row, Column } from '@/index';
 
 // CSF format story
 export const defaultMetric = () => {
   const [value, setValue] = React.useState(15);
 
   return (
-    <div className="d-flex align-items-center">
+    <Row className="align-items-center">
       <Label htmlFor="metric-input" className="mr-5">
         No. of Days
       </Label>
-      <div style={{ width: 'var(--spacing-6)' }}>
+      <Column size={1}>
         <MetricInput
           id="metric-input"
           name="metric-input"
@@ -20,8 +20,8 @@ export const defaultMetric = () => {
             setValue(e.target.value);
           }}
         />
-      </div>
-    </div>
+      </Column>
+    </Row>
   );
 };
 
@@ -29,9 +29,9 @@ const customCode = `() => {
   const [value, setValue] = React.useState(15);
 
   return (
-    <div className="d-flex align-items-center">
+    <Row className="align-items-center">
       <Label htmlFor="metric-input" className="mr-5">  No. of Days </Label>
-      <div style={{ width: 'var(--spacing-6)' }}>
+      <Column size={1}>
         <MetricInput
           id="metric-input"
           name="metric-input"
@@ -39,8 +39,8 @@ const customCode = `() => {
           value={value}
           onChange={e => { setValue(e.target.value); }}
         />
-      </div>
-    </div>
+        </Column>
+    </Row>
   );
 }`;
 
--- core/components/atoms/metricInput/__stories__/variants/Size.story.jsx
+++ core/components/atoms/metricInput/__stories__/variants/Size.story.jsx
@@ -1,18 +1,20 @@
 import * as React from 'react';
-import { MetricInput, Text } from '@/index';
+import { MetricInput, Text, Row, Column } from '@/index';
 
 // CSF format story
 export const size = () => (
-  <div className="d-flex" style={{ width: 'var(--spacing-8)' }}>
-    <div className="d-flex flex-column align-items-center mr-8">
-      <MetricInput aria-label="Metric input regular size" size="regular" className="mb-3" />
-      <Text>Regular</Text>
-    </div>
-    <div className="d-flex flex-column align-items-center">
-      <MetricInput aria-label="Metric input large size" size="large" className="mb-3" />
-      <Text>Large</Text>
-    </div>
-  </div>
+  <Row>
+    <Column size={2} className="d-flex">
+      <div className="flex-column align-items-center mr-8">
+        <MetricInput aria-label="Metric input regular size" size="regular" className="mb-3" />
+        <Text>Regular</Text>
+      </div>
+      <div className="flex-column align-items-center">
+        <MetricInput aria-label="Metric input large size" size="large" className="mb-3" />
+        <Text>Large</Text>
+      </div>
+    </Column>
+  </Row>
 );
 
 export default {
