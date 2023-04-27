13fcc715c61a3bb95aa78ef429b45b3b82af8a29
Anuradha Aggarwal <I1333@IT002600.local>
2023-04-27 19:11:46 +0530

    fix(switch): fix stories for switch component

--- core/components/atoms/switchInput/__stories__/variants/State.story.jsx
+++ core/components/atoms/switchInput/__stories__/variants/State.story.jsx
@@ -1,22 +1,21 @@
 import * as React from 'react';
-import Switch from '../../Switch';
-import Text from '@/components/atoms/text';
+import { Switch, Text } from '@/index';
 
 // CSF format story
 export const state = () => {
   return (
-    <div className="d-flex">
-      <div className="mr-9">
-        <Switch aria-label="Switch enabled" defaultChecked={true} disabled={false} />
-        <br />
-        <Text weight="strong">Enabled</Text>
+    <>
+      <div className="d-flex mb-7 justify-content-between w-75">
+        <Switch aria-label="Switch enabled" defaultChecked={true} />
+        <Switch aria-label="Switch disabled" defaultChecked={true} disabled={true} />
+        <Switch aria-label="Switch disabled" disabled={true} />
       </div>
-      <div>
-        <Switch aria-label="Switch disabled" disabled={true} defaultChecked={true} />
-        <br />
+      <div className="d-flex justify-content-between w-75">
+        <Text weight="strong">Enabled</Text>
+        <Text weight="strong">Enabled - Disabled</Text>
         <Text weight="strong">Disabled</Text>
       </div>
-    </div>
+    </>
   );
 };
 
@@ -27,11 +26,7 @@ export default {
     docs: {
       docPage: {
         title: 'Switch',
-        a11yProps: ` **aria-label:**
-        <br/> 
-        - Add \`aria-label='Switch enabled'\` on switch with *Enabled* text to describe the action of switch.
-        <br/>
-        - Add \`aria-label='Switch disabled'\` on switch with *Disabled* text to describe the action of switch.`,
+        a11yProps: ` **aria-label:**<br/> - Add \`aria-label='Switch enabled'\` on switch with *Enabled* text to describe the action of switch.<br/> - Add \`aria-label='Switch disabled'\` on switch with *Disabled* text to describe the action of switch.`,
       },
     },
   },
