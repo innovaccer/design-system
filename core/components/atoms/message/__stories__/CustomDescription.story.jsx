51ed5541d2f5e2b17c5deca700ba94b531279f17
Satyam Chatterjee <satyam.chatterjee@innovaccer.com>
2023-06-09 15:23:30 +0530

    fix(message): update action link to linkButton

--- core/components/atoms/message/__stories__/CustomDescription.story.jsx
+++ core/components/atoms/message/__stories__/CustomDescription.story.jsx
@@ -1,18 +1,9 @@
 import * as React from 'react';
-import { Message, Text } from '@/index';
+import { Message, Text, LinkButton } from '@/index';
 
 export const CustomDescription = () => (
-  <Message
-    className="w-75"
-    actions={
-      <>
-        <Text className="cursor-pointer" appearance="link">
-          Try again
-        </Text>
-      </>
-    }
-  >
-    <Text appearance="default">Sorry we couldn't subscribe you. Please try again.</Text>
+  <Message className="w-75" actions={<LinkButton>Try again</LinkButton>}>
+    <Text>Sorry we couldn't subscribe you. Please try again.</Text>
   </Message>
 );
 
