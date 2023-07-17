aaa286f5eb441ada682b8c3b000b175ee8dfcb73
Satyam Chatterjee <satyam.chatterjee@innovaccer.com>
2023-07-17 17:23:46 +0530

    feat(storybook): add sandbox feature within storybook

--- core/components/patterns/datePicker/dateAndTimePicker.story.tsx
+++ core/components/patterns/datePicker/dateAndTimePicker.story.tsx
@@ -2,79 +2,29 @@ import * as React from 'react';
 
 export const dateAndTimePicker = () => <></>;
 
-const customCode = `
-
-() => {
-  class DateTimePicker extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        open: false,
-        date: undefined,
-        time: undefined
-      };
-    }
-
-    onDateChange(date, dateVal) {
-      this.setState({
-        date: dateVal,
-        open: !!date
-      });
-    }
-
-    onPopperToggle(val) {
-      this.setState({
-        open: val
-      });
-    }
-
-    onChangeHandler(selected) {
-      this.setState({
-        time: selected
-      });
-    }
-
-    render() {
-      const timeValues = ['10:00 AM', '10:30 AM', '11:00 AM', '11:30 AM', '12:00 PM', '12:30 PM', '01:00 PM', '01:30 PM'];
-      const {
-        open
-      } = this.state;
-
-      console.log("Date", this.state.date, "Time", this.state.time);
-
-      return (
-        <div className="d-flex">
-          <div className="d-flex flex-column">
-            <Label withInput>Date</Label>
-            <DatePicker
-              withInput={true}
-              onDateChange={this.onDateChange.bind(this)}
-            />
-          </div>
-          <div className="d-flex flex-column ml-5" style={{width: 'var(--spacing-8)'}}>
-            <Label withInput>Time</Label>
-            <Dropdown
-              open={open}
-              onPopperToggle={this.onPopperToggle.bind(this)}
-              options={timeValues.map(value => ({label: value, value}))}
-              onChange={this.onChangeHandler.bind(this)}
-            />
-          </div>
-        </div>
-      );
-    }
-  }
-
-  return <DateTimePicker />
-}`;
+const sandbox = (
+  <iframe
+    src="https://codesandbox.io/embed/angry-tdd-97ghqj?autoresize=1&fontsize=14&hidenavigation=1&theme=dark"
+    style={{ width: '100%', height: '500px', border: '4px', borderRadius: '4px', overflow: 'hidden' }}
+    title="angry-tdd-97ghqj"
+    allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+    sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+  />
+);
 
 export default {
   title: 'Patterns/DatePicker/Date And Time Picker',
   parameters: {
+    previewTabs: {
+      canvas: {
+        hidden: true,
+      },
+    },
     docs: {
       docPage: {
-        customCode,
         noProps: true,
+        noStory: true,
+        embedSandbox: sandbox,
         title: 'Date and time picker',
       },
     },
--- core/components/patterns/datePicker/datePickerWithPresets.story.tsx
+++ core/components/patterns/datePicker/datePickerWithPresets.story.tsx
@@ -1,124 +1,32 @@
 import * as React from 'react';
 import { DatePicker } from '@/index';
 
-const customCode = `() => {
-
-  const setDate = (jumpDateIndex) => {
-    const d = new Date();
-    return d.setDate(d.getDate() + jumpDateIndex);
-  };
-
-  const getTomorrowDate = setDate.bind(null, 1);
-  const getThreeDaysLaterDate = setDate.bind(null, 3);
-  const getOneWeekLaterDate = setDate.bind(null, 7);
-  const getThirtyDaysLaterDate = setDate.bind(null, 30);
-
-  
-  const DatePickerPreset = ({ size, withInput=false }) => {
-    
-    const [date, setDate] = React.useState(new Date());
-    const [selectedChip, setSelectedChip] = React.useState('today');
-    
-    const classNames = size === 'small' ? 'd-block mb-4' : 'd-block mb-5';
-    return (
-      <DatePicker date={date} showTodayChip={false} size={size} withInput={withInput}>
-        <div className="pt-6 px-5">
-          <div className="d-flex align-items-center justify-content-between">
-            <Subheading size="s" appearance="subtle">
-              Date
-            </Subheading>
-          </div>
-          <div className="pt-4">
-            <Chip
-              label="Today"
-              clearButton={false}
-              type="selection"
-              className={classNames}
-              selected={selectedChip === 'today'}
-              name="rangePicker"
-              onClick={() => {
-                setDate(new Date)
-                setSelectedChip('today')
-              }}
-            />
-            <Chip
-              label="Tomorrow"
-              clearButton={false}
-              type="selection"
-              className={classNames}
-              selected={selectedChip === 'tomorrow'}
-              name={'chip'}
-              onClick={() => {
-                setDate(getTomorrowDate())
-                setSelectedChip('tomorrow')
-              }}
-            />
-            <Chip
-              label="3 Days later"
-              clearButton={false}
-              type="selection"
-              className={classNames}
-              selected={selectedChip === 'threeDaysLater'}
-              name="rangePicker"
-              onClick={() => {
-                setDate(getThreeDaysLaterDate())
-                setSelectedChip('threeDaysLater')
-              }}
-            />
-            <Chip
-              label="1 Week later"
-              clearButton={false}
-              type="selection"
-              className={classNames}
-              selected={selectedChip === 'oneWeekLater'}
-              name="rangePicker"
-              onClick={() => {
-                setDate(getOneWeekLaterDate())
-                setSelectedChip('oneWeekLater')
-              }}
-            />
-            <Chip
-              label="30 Days later"
-              clearButton={false}
-              type="selection"
-              className={classNames}
-              selected={selectedChip === 'thirtyDaysLater'}
-              name="rangePicker"
-              onClick={() => {
-                setDate(getThirtyDaysLaterDate())
-                setSelectedChip('thirtyDaysLater')
-              }}
-            />
-          </div>
-        </div>
-        <Divider vertical={true} />
-      </DatePicker>
-    );
-  };
-  return (
-    <>
-      <Card className="d-inline-flex" shadow="light">
-        <DatePickerPreset />
-      </Card>
-      <Card className="d-inline-flex ml-5" shadow="light">
-        <DatePickerPreset size="small" />
-      </Card>
-      <Card className="w-50 my-5 p-5">
-        <Label>Set an appointment date:</Label>
-        <DatePickerPreset withInput={true} />
-      </Card>
-    </>
-  );
-};`;
 export const WithPresets = () => <></>;
+
+const sandbox = (
+  <iframe
+    src="https://codesandbox.io/embed/bold-rosalind-q68jqz?autoresize=1&fontsize=14&hidenavigation=1&theme=dark"
+    style={{ width: '100%', height: '500px', border: '0', borderRadius: '4px', overflow: 'hidden' }}
+    title="bold-rosalind-q68jqz"
+    allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+    sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+  />
+);
 
 export default {
   title: 'Patterns/DatePicker/With Presets',
   component: DatePicker,
   parameters: {
+    previewTabs: {
+      canvas: {
+        hidden: true,
+      },
+    },
     docs: {
       docPage: {
-        customCode,
+        noProps: true,
+        noStory: true,
+        embedSandbox: sandbox,
         title: 'DatePicker',
       },
     },
