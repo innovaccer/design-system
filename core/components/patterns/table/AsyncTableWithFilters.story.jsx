210bb99bc3aa98e78054b5b0321885351ea0c7bb
Anuradha Aggarwal <I1333@IT002600.local>
2023-08-07 12:03:07 +0530

    feat(table): embed codesandbox for table filter pattern in storybook

---
+++ core/components/patterns/table/AsyncTableWithFilters.story.jsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+export const asyncTableWithFilters = () => <></>;
+
+export default {
+  title: 'Patterns/Table/Async Table With Filters',
+  parameters: {
+    previewTabs: {
+      canvas: {
+        hidden: true,
+      },
+    },
+    docs: {
+      docPage: {
+        noProps: true,
+        noStory: true,
+        sandboxTitle: 'async-table-filters-pattern-z56dp6?file=/src/MainTable.js',
+        title: 'Async Table With Filters',
+      },
+    },
+  },
+};
