76ba6e936e36fcf9bae678d8e90d5fcce6ec3f46
Ayushi Teotia <ayushi.teotia@innovaccer.com>
2023-10-25 17:11:45 +0530

    docs(dcos): add donut chart in visualizations, change tab name

--- docs/src/data/components/visualizations.js
+++ docs/src/data/components/visualizations.js
@@ -12,13 +12,20 @@ export const data = [
     design: 'Available',
     name: 'Bar charts',
     code: 'Available',
-    image: () => <StaticImage src="./images/patterns/email.png" alt="Email" imgStyle={imgStyle} />,
+    image: () => <StaticImage src="./images/visualizations/barchart.png" alt="Bar chart" imgStyle={imgStyle} />,
+  },
+  {
+    link: 'donutChart/usage',
+    design: 'Available',
+    name: 'Donut chart',
+    code: 'Available',
+    image: () => <StaticImage src="./images/visualizations/donutchart.png" alt="Donut chart" imgStyle={imgStyle} />,
   },
   {
     link: 'lineChart/usage',
     design: 'Available',
     name: 'Line chart',
     code: 'Available',
-    image: () => <StaticImage src="./images/patterns/email.png" alt="Email" imgStyle={imgStyle} />,
+    image: () => <StaticImage src="./images/visualizations/linechart.png" alt="Line chart" imgStyle={imgStyle} />,
   }
 ];
