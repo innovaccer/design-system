facc7aacab2ca4650bae02eb33a795a5f42e7b20
anuradha9712 <anuradha.mcs18.du@gmail.com>
2023-11-03 13:59:05 +0400

    fix(docs): fix data visualization link and navigation

--- docs/src/data/components/visualizations.js
+++ docs/src/data/components/visualizations.js
@@ -36,3 +36,47 @@ export const data = [
     image: () => <StaticImage src="./images/visualizations/linechart.png" alt="Line chart" imgStyle={imgStyle} />,
   }
 ];
+
+export const schema = [
+  {
+    name: 'name',
+    displayName: 'Component Name',
+    width: '40%',
+    sorting: false,
+    cellRenderer: ({ data }) => {
+      return (
+        <Link className="Text--link card-link" to={`/patterns/${data.link}`}>
+          {data.name}
+        </Link>
+      );
+    },
+  },
+  {
+    name: 'design',
+    displayName: 'Design',
+    width: '30%',
+    cellType: 'STATUS_HINT',
+    sorting: false,
+    translate: (a) => {
+      const design = a.design;
+      return {
+        title: design,
+        statusAppearance: design === 'Available' ? 'success' : design === 'Unavailable' ? 'alert' : 'info',
+      };
+    },
+  },
+  {
+    name: 'code',
+    displayName: 'Code',
+    width: '30%',
+    cellType: 'STATUS_HINT',
+    sorting: false,
+    translate: (a) => {
+      const code = a.code;
+      return {
+        title: code,
+        statusAppearance: code === 'Available' ? 'success' : code === 'Unavailable' ? 'alert' : 'info',
+      };
+    },
+  },
+];
