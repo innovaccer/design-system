87ad7ae63278dfeeca5ed76beefcde5b45b49f50
samyak3009 <samyak3009@gmail.com>
2024-05-31 15:00:46 +0400

    feat(chipinput): add overflow behaviour in chipinput

--- core/components/atoms/_chip/index.tsx
+++ core/components/atoms/_chip/index.tsx
@@ -5,6 +5,7 @@ import Text from '@/components/atoms/text';
 import { Name } from '../chip/Chip';
 import { BaseProps, extractBaseProps } from '@/utils/types';
 import { IconProps, TextProps } from '@/index.type';
+import { Tooltip } from '@/index';
 import { IconType } from '@/common.type';
 
 export interface GenericChipProps extends BaseProps {
@@ -18,10 +19,21 @@ export interface GenericChipProps extends BaseProps {
   onClick?: () => void;
   iconType?: IconType;
   name: Name;
+  maxWidth: string | number;
 }
 
 export const GenericChip = (props: GenericChipProps) => {
-  const { label, icon, clearButton, disabled, className, selected, onClose, onClick, labelPrefix, iconType } = props;
+  const { label, icon, clearButton, disabled, className, selected, onClose, onClick, labelPrefix, iconType, maxWidth } =
+    props;
+  const maxWidthStyle = { maxWidth: maxWidth };
+  const [isTextTruncated, setIsTextTruncated] = React.useState(false);
+  const { detectTruncation } = Tooltip.useAutoTooltip();
+  const contentRef = React.createRef<HTMLDivElement>();
+
+  React.useEffect(() => {
+    const isTruncated = detectTruncation(contentRef);
+    setIsTextTruncated(isTruncated);
+  }, [contentRef]);
 
   const baseProps = extractBaseProps(props);
 
@@ -68,7 +80,7 @@ export const GenericChip = (props: GenericChipProps) => {
   const renderLabel = () => {
     if (typeof label === 'string') {
       return (
-        <>
+        <div className="Chip-text--truncate" ref={contentRef}>
           {labelPrefix && (
             <Text
               data-test="DesignSystem-GenericChip--LabelPrefix"
@@ -82,49 +94,67 @@ export const GenericChip = (props: GenericChipProps) => {
           <Text data-test="DesignSystem-GenericChip--Text" color={textColor} className="Chip-text">
             {label}
           </Text>
-        </>
+        </div>
       );
     }
     return label;
   };
 
   return (
-    // TODO(a11y)
-    // eslint-disable-next-line
-    <div
-      // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
-      tabIndex={disabled ? -1 : 0}
-      data-test="DesignSystem-GenericChip--GenericChipWrapper"
-      {...baseProps}
-      className={`Chip-wrapper ${className}`}
-      onClick={onClickHandler}
-    >
-      {icon && (
-        <Icon
-          data-test="DesignSystem-GenericChip--Icon"
-          name={icon}
-          type={iconType}
-          appearance={iconAppearance('left')}
-          className={iconClass('left')}
-        />
-      )}
-      {renderLabel()}
-      {clearButton && (
-        <div
-          role="button"
-          onClick={onCloseHandler}
-          tabIndex={disabled ? -1 : 0}
-          onKeyDown={onKeyDownHandler}
-          className={iconClass('right')}
-          data-test="DesignSystem-GenericChip--clearButton"
-        >
-          <Icon name="clear" appearance={iconAppearance('right')} className="p-2" />
-        </div>
-      )}
+    <div>
+      <Tooltip
+        showTooltip={isTextTruncated}
+        data-test="DesignSystem-GenericChip--Tooltip"
+        tooltip={
+          labelPrefix
+            ? `${labelPrefix} ${typeof label === 'string' ? label : ''}`
+            : typeof label === 'string'
+            ? label
+            : ''
+        }
+      >
+        <>
+          {/* eslint-disable-next-line jsx-a11y/no-static-element-interactions, jsx-a11y/click-events-have-key-events */}
+          <div
+            tabIndex={disabled ? -1 : 0}
+            style={maxWidthStyle}
+            data-test="DesignSystem-GenericChip--GenericChipWrapper"
+            {...baseProps}
+            className={`Chip-wrapper ${className}`}
+            onClick={onClickHandler}
+          >
+            {icon && (
+              <Icon
+                data-test="DesignSystem-GenericChip--Icon"
+                name={icon}
+                type={iconType}
+                appearance={iconAppearance('left')}
+                className={iconClass('left')}
+              />
+            )}
+            {renderLabel()}
+            {clearButton && (
+              <div
+                role="button"
+                onClick={onCloseHandler}
+                tabIndex={disabled ? -1 : 0}
+                onKeyDown={onKeyDownHandler}
+                className={iconClass('right')}
+                data-test="DesignSystem-GenericChip--clearButton"
+              >
+                <Icon name="clear" appearance={iconAppearance('right')} className="p-2" />
+              </div>
+            )}
+          </div>
+        </>
+      </Tooltip>
     </div>
   );
 };
 
 GenericChip.displayName = 'GenericChip';
+GenericChip.defaultProps = {
+  maxWidth: '256px',
+};
 
 export default GenericChip;
---
+++ core/components/molecules/editableChipInput/__stories__/OverflowBehaviour.story.jsx
@@ -0,0 +1,81 @@
+import * as React from 'react';
+import { action } from '@/utils/action';
+import { EditableChipInput, Label } from '@/index';
+
+// CSF format story
+
+export const overflowBehaviour = () => {
+  const [value, setValue] = React.useState(['John', 'Eric', 'Martin', 'Joy']);
+
+  const onChange = (updatedValue) => {
+    setValue(updatedValue);
+  };
+  const onClick = (item) => action(`onClick: ${item}`);
+
+  const placeholder = 'Add Value';
+  const chipOptions = { onClick, clearButton: true };
+  const chipInputOptions = {
+    chipOptions,
+    allowDuplicates: false,
+    defaultValue: [],
+    autoFocus: true,
+  };
+
+  const options = {
+    placeholder,
+    value,
+    onChange,
+    chipInputOptions,
+  };
+  return (
+    <div className="w-25">
+      <Label>Members</Label>
+      <EditableChipInput {...options} />
+    </div>
+  );
+};
+
+const customCode = `() => {
+  const [value, setValue] = React.useState(["John","Eric","Martin", "Joy"]);
+
+  const onChange = (updatedValue) => {
+    setValue(updatedValue);
+  };
+  const onClick = (item) =>  console.log(item);
+
+  const placeholder ='Add Value';
+  const chipOptions = { onClick, clearButton:true };
+  const chipInputOptions = {
+    chipOptions,
+    allowDuplicates:false,
+    defaultValue:[],
+    autoFocus:true
+  };
+
+  const options = {
+    placeholder,
+    value,
+    onChange,
+    chipInputOptions
+  };
+  return (
+      <div className="w-25">
+        <Label>Members</Label>
+        <EditableChipInput
+          {...options}
+        />
+      </div>
+  );
+}`;
+
+export default {
+  title: 'Components/Inline Editable Fields/EditableChipInput/Overflow Behaviour',
+  component: EditableChipInput,
+  parameters: {
+    docs: {
+      docPage: {
+        customCode,
+      },
+    },
+  },
+};
