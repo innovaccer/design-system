f3eaf718d92d4f47d2b764c5a787d2dbe4ae82b3
anuradha9712 <anuradha.mcs18.du@gmail.com>
2024-06-03 17:38:18 +0400

    feat(progressIndicator): add new progress indicator pattern

---
+++ core/components/patterns/progressIndicator/horizontalProgressIndicator.story.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { SaraSparkle, Text } from '@/index';
+
+export const Horizontal = () => {
+  return (
+    <div className="d-flex align-items-center justify-content-center">
+      <SaraSparkle size={24} state="listening" className="mr-3" />
+      <Text className="shimmer-text">Sara is listening</Text>
+    </div>
+  );
+};
+
+export default {
+  title: 'Patterns/ProgressIndicator/Horizontal',
+  parameters: {
+    docs: {
+      docPage: {
+        title: 'Progress Indicator - Horizontal',
+        noProps: true,
+      },
+    },
+  },
+};
