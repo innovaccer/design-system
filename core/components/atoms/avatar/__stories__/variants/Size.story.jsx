b974994954fd2e1f3ace78b497a5e925a5effc22
anuradha9712 <anuradha.mcs18.du@gmail.com>
2024-06-11 17:16:20 +0400

    docs(avatar): update documentation of avatar component

--- core/components/atoms/avatar/__stories__/variants/Size.story.jsx
+++ core/components/atoms/avatar/__stories__/variants/Size.story.jsx
@@ -1,37 +1,27 @@
 import * as React from 'react';
-import { Avatar, Row, Text, Column } from '@/index';
+import { Avatar, Row, Column, Text } from '@/index';
 
 // CSF format story
 export const size = () => {
-  const weight = 'strong';
-
   return (
-    <Row className="w-50">
-      <Column>
-        <Text weight={weight}>Regular</Text>
-        <br />
-        <br />
-        <Avatar firstName="John" lastName="Doe" />
-      </Column>
-      <Column>
-        <Text weight={weight}>Tiny</Text>
-        <br />
-        <br />
-        <Avatar firstName="John" lastName="Doe" size="tiny" />
-      </Column>
-      <Column>
-        <Text weight={weight}>Regular</Text>
-        <br />
-        <br />
-        <Avatar appearance="accent4" firstName="John" lastName="Doe" shape="square" />
-      </Column>
-      <Column>
-        <Text weight={weight}>Tiny</Text>
-        <br />
-        <br />
-        <Avatar appearance="accent4" firstName="John" lastName="Doe" size="tiny" shape="square" />
-      </Column>
-    </Row>
+    <div>
+      <Row>
+        <Column>
+          <Text weight="strong">Regular</Text>
+          <div className="d-flex mt-7">
+            <Avatar firstName="John" lastName="Doe" className="mr-8" />
+            <Avatar appearance="accent4" firstName="John" lastName="Doe" shape="square" />
+          </div>
+        </Column>
+        <Column>
+          <Text weight="strong">Tiny</Text>
+          <div className="d-flex mt-7">
+            <Avatar firstName="John" lastName="Doe" size="tiny" className="mr-8" />
+            <Avatar appearance="accent4" firstName="John" lastName="Doe" shape="square" size="tiny" />
+          </div>
+        </Column>
+      </Row>
+    </div>
   );
 };
 
