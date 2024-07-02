d31d5fd19382afcb7570bf200f363902d9b527e7
samyak3009 <samyak3009@gmail.com>
2024-07-02 13:22:02 +0400

    feat(Message): update background colors in message

--- core/components/atoms/message/__stories__/appearance/Info.story.jsx
+++ core/components/atoms/message/__stories__/appearance/Info.story.jsx
@@ -3,7 +3,7 @@ import { Message } from '@/index';
 
 // CSF format story
 export const info = () => (
-  <Message className="w-75" appearance="info" description="Patient profile has been updated with new records." />
+  <Message className="w-50" appearance="info" description="Patient profile has been updated with new records." />
 );
 
 export default {
