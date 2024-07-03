ada64018429e06a751225b2358b4f4222e62fbf7
anuradha9712 <anuradha.mcs18.du@gmail.com>
2024-07-03 14:34:52 +0400

    feat(figma): add figma code connect to atomic components

--- figma/Avatar.figma.tsx
+++ figma/Avatar.figma.tsx
@@ -2,32 +2,37 @@ import React from 'react';
 import { Avatar } from '@/index';
 import figma from '@figma/code-connect';
 
-figma.connect(
-  Avatar,
-  'https://www.figma.com/design/w8sqBtJpvq86D06UE7gN0T/branch/0LPEpuAwooHbyZ1UgPWZfq/MDS---Web?node-id=37-592&m=dev',
-  {
-    imports: ["import { Avatar } from '@innovaccer/design-system'"],
-    props: {
-      appearance: figma.enum('Appearance', {
-        Jal: 'primary',
-        Stone: 'secondary',
-        Neem: 'success',
-        Haldi: 'warning',
-        Mirch: 'alert',
-        Tawak: 'accent1',
-        Nimbu: 'accent4',
-        Neel: 'accent3',
-        Jamun: 'accent2',
-      }),
-      size: figma.enum('Size', {
-        Regular: 'regular',
-        Small: 'tiny',
-      }),
-      shape: figma.enum('Shape', {
-        Round: 'round',
-        Square: 'square',
-      }),
-    },
-    example: (props) => <Avatar {...props} />,
-  }
-);
+figma.connect(Avatar, 'https://www.figma.com/design/w8sqBtJpvq86D06UE7gN0T/MDS---Web?node-id=37-592', {
+  imports: ["import { Avatar } from '@innovaccer/design-system'"],
+  props: {
+    appearance: figma.enum('Appearance', {
+      Jal: 'primary',
+      Stone: 'secondary',
+      Neem: 'success',
+      Haldi: 'warning',
+      Mirch: 'alert',
+      Tawak: 'accent1',
+      Nimbu: 'accent4',
+      Neel: 'accent3',
+      Jamun: 'accent2',
+    }),
+    size: figma.enum('Size', {
+      Regular: 'regular',
+      Small: 'tiny',
+    }),
+    shape: figma.enum('Shape', {
+      Round: 'round',
+      Square: 'square',
+    }),
+    firstName: figma.enum('Type', {
+      Text: 'John',
+    }),
+    lastName: figma.enum('Type', {
+      Text: 'Doe',
+    }),
+    disabled: figma.enum('State', {
+      Disabled: true,
+    }),
+  },
+  example: (props) => <Avatar {...props} />,
+});
---
+++ figma/Divider.figma.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Divider } from '@/index';
+import figma from '@figma/code-connect';
+
+// Horizontal Divider
+figma.connect(Divider, 'https://www.figma.com/design/w8sqBtJpvq86D06UE7gN0T/MDS---Web?node-id=39-336', {
+  imports: ["import { Divider } from '@innovaccer/design-system'"],
+  props: {
+    appearance: figma.enum('Type', {
+      Default: 'basic',
+      'Header (Darker)': 'header',
+    }),
+  },
+  example: (props) => <Divider {...props} />,
+});
+
+// Vertical Divider
+figma.connect(Divider, 'https://www.figma.com/design/w8sqBtJpvq86D06UE7gN0T/MDS---Web?node-id=670-1648', {
+  imports: ["import { Divider } from '@innovaccer/design-system'"],
+  example: (props) => <Divider vertical={true} {...props} />,
+});
