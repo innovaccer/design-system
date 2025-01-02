59d2553add1192c4545b20ffd6e25e73bb788c5b
anuradha9712 <anuradha.mcs18.du@gmail.com>
2025-01-02 20:09:54 +0400

    chore: update design system esm bundle path in docs script

--- docs/tools/update-mds.sh
+++ docs/tools/update-mds.sh
@@ -1,4 +1,4 @@
 # !bin/sh
 
-sed -i.bak 's/boundaryElement: document.body/boundaryElement: {}/' node_modules/@innovaccer/design-system/dist/index.esm.js
-sed -i.bak 's/boundariesElement: boundaryElement || document.body/boundaryElement: {}/' node_modules/@innovaccer/design-system/dist/index.esm.js
+sed -i.bak 's/boundaryElement: document.body/boundaryElement: {}/' node_modules/@innovaccer/design-system/dist/esm/index.js
+sed -i.bak 's/boundariesElement: boundaryElement || document.body/boundaryElement: {}/' node_modules/@innovaccer/design-system/dist/esm/index.js
