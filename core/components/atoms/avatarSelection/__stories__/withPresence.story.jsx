85ad31244bd3ff84a0484ec57ab95203d399e479
anuradha9712 <anuradha.mcs18.du@gmail.com>
2025-01-14 21:15:26 +0400

    feat(avatarSelection): add status & presence support in avatar selection

---
+++ core/components/atoms/avatarSelection/__stories__/withPresence.story.jsx
@@ -0,0 +1,173 @@
+import * as React from 'react';
+import { action } from '@/utils/action';
+import { AvatarSelection, Avatar, Input } from '@/index';
+
+export const withPresence = () => {
+  const list = [
+    {
+      firstName: 'John',
+      lastName: 'Doe',
+      selected: true,
+    },
+    {
+      firstName: 'Anuradha',
+      lastName: 'Aggarwal',
+      image: <Avatar.Image src="https://design.innovaccer.com/images/avatar2.jpeg" />,
+      presence: 'active',
+    },
+    {
+      firstName: 'Nancy',
+      lastName: 'Wheeler',
+    },
+    {
+      firstName: 'Monica',
+      lastName: 'Geller',
+      presence: 'away',
+    },
+    {
+      firstName: 'Arya',
+      lastName: 'Stark',
+    },
+    {
+      firstName: 'Rachel',
+      lastName: 'Green',
+      icon: <Avatar.Icon name="person" />,
+    },
+    {
+      firstName: 'Satyam',
+      lastName: 'Yadav',
+      selected: true,
+      presence: 'away',
+      image: <Avatar.Image src="https://design.innovaccer.com/images/avatar1.png" />,
+    },
+    {
+      firstName: 'Walter',
+      lastName: 'Wheeler',
+      selected: true,
+    },
+    {
+      firstName: 'Monica',
+      lastName: 'Geller',
+    },
+    {
+      firstName: 'Arya',
+      lastName: 'Stark',
+      presence: 'away',
+    },
+  ];
+
+  const onSelectHandler = (props) => {
+    action('props', props)();
+  };
+
+  const searchComparator = (searchValue, data) => {
+    if (searchValue === '') {
+      return true;
+    }
+    return data.firstName.toLowerCase().includes(searchValue.toLowerCase());
+  };
+
+  return (
+    <AvatarSelection
+      list={list}
+      withSearch={true}
+      onSelect={onSelectHandler}
+      searchPlaceholder="Search User"
+      searchComparator={searchComparator}
+    />
+  );
+};
+
+const customCode = `() => {
+  const list = [
+    {
+      firstName: 'John',
+      lastName: 'Doe',
+      selected: true,
+    },
+    {
+      firstName: 'Anuradha',
+      lastName: 'Aggarwal',
+      image: <Avatar.Image src="https://design.innovaccer.com/images/avatar2.jpeg" />,
+      presence: 'active',
+    },
+    {
+      firstName: 'Nancy',
+      lastName: 'Wheeler',
+    },
+    {
+      firstName: 'Monica',
+      lastName: 'Geller',
+      presence: 'away',
+    },
+    {
+      firstName: 'Arya',
+      lastName: 'Stark',
+    },
+    {
+      firstName: 'Rachel',
+      lastName: 'Green',
+      icon: <Avatar.Icon name="person" />,
+    },
+    {
+      firstName: 'Satyam',
+      lastName: 'Yadav',
+      selected: true,
+      presence: 'away',
+      image: <Avatar.Image src="https://design.innovaccer.com/images/avatar1.png" />,
+    },
+    {
+      firstName: 'Walter',
+      lastName: 'Wheeler',
+      selected: true,
+    },
+    {
+      firstName: 'Monica',
+      lastName: 'Geller',
+    },
+    {
+      firstName: 'Arya',
+      lastName: 'Stark',
+      presence: 'away',
+    },
+  ];
+
+  const onSelectHandler = (props) => {
+    console.log('props', props);
+  };
+
+  const searchComparator = (searchValue, data) => {
+    if (searchValue === '') {
+      return true;
+    }
+    return data.firstName.toLowerCase().includes(searchValue.toLowerCase());
+  };
+
+  return (
+    <AvatarSelection
+      list={list}
+      withSearch={true}
+      onSelect={onSelectHandler}
+      searchPlaceholder="Search User"
+      searchComparator={searchComparator}
+    />
+  );
+}`;
+
+export default {
+  title: 'Components/Avatar/AvatarSelection/With Presence',
+  component: AvatarSelection,
+  subcomponents: {
+    'AvatarSelection.Input': Input,
+    'AvatarSelection.List': AvatarSelection.List,
+    'AvatarSelection.Option': AvatarSelection.Option,
+    'AvatarSelection.EmptyState': AvatarSelection.EmptyState,
+  },
+  parameters: {
+    docs: {
+      docPage: {
+        customCode,
+      },
+    },
+  },
+};
