5af38b766ee6f51ad9ea84d8aa63e1806174fcf0
anuradha9712 <anuradha.mcs18.du@gmail.com>
2025-01-24 09:41:12 +0400

    feat(avatarGroup): add figma code connect for avatar group component

---
+++ figma/AvatarGroup.figma.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Avatar, AvatarGroup } from '@/index';
+import figma from '@figma/code-connect';
+
+figma.connect(AvatarGroup, 'https://www.figma.com/design/w8sqBtJpvq86D06UE7gN0T/MDS---Web?node-id=37-593', {
+  imports: ["import { AvatarGroup } from '@innovaccer/design-system'"],
+  props: {
+    size: figma.enum('Size', {
+      Regular: 'regular',
+      Small: 'tiny',
+    }),
+    max: figma.enum('Count', {
+      'Less than three': 3,
+      'More than three': 2,
+    }),
+    list: figma.enum('Type', {
+      Initial: [
+        {
+          firstName: 'John',
+          lastName: 'Doe',
+        },
+        {
+          firstName: 'Jane',
+          lastName: 'Doe',
+        },
+      ],
+      Icon: [
+        {
+          firstName: 'Rachel',
+          lastName: 'Green',
+          icon: <Avatar.Icon name="person" />,
+        },
+        {
+          firstName: 'John',
+          lastName: 'Doe',
+          icon: <Avatar.Icon name="person" />,
+        },
+      ],
+      Image: [
+        {
+          firstName: 'John',
+          lastName: 'Doe',
+          image: <Avatar.Image src="url" />,
+        },
+        {
+          firstName: 'Rachel',
+          lastName: 'Green',
+          image: <Avatar.Image src="url" />,
+        },
+      ],
+    }),
+  },
+  example: (props) => <AvatarGroup {...props} />,
+});
