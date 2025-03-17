b617cb2798d762a80e7730b827254e8c3293e3c8
anuradha9712 <anuradha.mcs18.du@gmail.com>
2025-03-18 04:57:37 +0530

    feat(AIResponse): update AI Response component design and API

--- core/ai-components/AIResponse/__stories__/custom.story.jsx
+++ core/ai-components/AIResponse/__stories__/custom.story.jsx
@@ -1,28 +1,53 @@
 import React from 'react';
-import { AIResponse, Sara, Text } from '@/index';
+import { AIResponse, Text } from '@/index';
 import Image from '../assets/media.png';
 
 export const CustomContent = () => {
+  const metaDataRenderer = () => {
+    return (
+      <Text appearance="subtle" size="small" weight="medium">
+        1:00 PM
+      </Text>
+    );
+  };
+
   return (
-    <div className="d-flex w-25">
-      <Sara />
-      <div className="ml-4">
-        <AIResponse>
-          <AIResponse.Body>
-            <Text>
-              Acute sinusitis causes the spaces inside the nose, known as sinuses, to become inflamed and swollen. Acute
-              sinusitis makes it hard for the sinuses to drain. Mucus builds up.
-            </Text>
-            <img alt="placeholder" src={Image} className="mt-4" />
-          </AIResponse.Body>
-        </AIResponse>
-        <Text appearance="subtle" size="small" weight="medium">
-          1:00 PM
+    <AIResponse showAvatar={true} metaData={metaDataRenderer} className="w-50">
+      <AIResponse.Body>
+        <Text>
+          Acute sinusitis causes the spaces inside the nose, known as sinuses, to become inflamed and swollen. Acute
+          sinusitis makes it hard for the sinuses to drain. Mucus builds up.
         </Text>
-      </div>
-    </div>
+        <img alt="placeholder" src={Image} className="mt-4" />
+      </AIResponse.Body>
+    </AIResponse>
   );
 };
+const ImagePath = JSON.stringify(Image);
+
+const customCode = `() => {
+
+  const metaDataRenderer = () => {
+    return (
+      <Text appearance="subtle" size="small" weight="medium">
+        1:00 PM
+      </Text>
+    );
+  };
+
+  return (
+    <AIResponse showAvatar={true} metaData={metaDataRenderer} className="w-50">
+      <AIResponse.Body>
+        <Text>
+          Acute sinusitis causes the spaces inside the nose, known as sinuses, to become inflamed and swollen. Acute
+          sinusitis makes it hard for the sinuses to drain. Mucus builds up.
+        </Text>
+        <img alt="placeholder" src={${ImagePath}} className="mt-4" />
+      </AIResponse.Body>
+    </AIResponse>
+  );
+  
+}`;
 
 export default {
   title: 'Components/AI/AI Response/Custom Content',
@@ -36,6 +61,7 @@ export default {
     docs: {
       docPage: {
         title: 'AIResponse',
+        customCode,
       },
     },
   },
