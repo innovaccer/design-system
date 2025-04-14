a8d2f1c1fe3bee4504dcfce5f106d36564a7db26
anuradha9712 <anuradha.mcs18.du@gmail.com>
2025-04-14 14:59:07 +0400

    feat(aibutton): update appearance with sparkle variant in AI Button component

--- figma/AIButton.figma.tsx
+++ figma/AIButton.figma.tsx
@@ -5,14 +5,11 @@ import figma from '@figma/code-connect';
 figma.connect(AIButton, 'https://www.figma.com/design/w8sqBtJpvq86D06UE7gN0T/MDS---Web?node-id=48860-174575', {
   imports: ["import { AIButton } from '@innovaccer/design-system'"],
   props: {
-    appearance: figma.enum('Appearance', {
-      Primary: 'primary',
-      Basic: 'basic',
-    }),
     disabled: figma.enum('State', {
       Disabled: true,
       Default: false,
     }),
+    withSparkle: figma.boolean('Sparkle'),
   },
   example: (props) => <AIButton {...props}>Button</AIButton>,
 });
