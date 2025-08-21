3ee86b88f31435ea4047d3707afcb1d33c92c825
veekays <vikaskumarsingh9838@gmail.com>
2025-08-21 17:36:24 +0400

    feat(message): add small variant support in message

--- core/components/atoms/message/Message.tsx
+++ core/components/atoms/message/Message.tsx
@@ -45,9 +45,9 @@ export type MessageProps = {
 } & BaseProps;
 
 export const Message = (props: MessageProps) => {
-  const { actions, title, className, size = 'regular' } = props;
   let { appearance = 'info' } = props;
   const { description = '' } = props;
+  const { actions, title, className, size = 'regular' } = props;
   appearance = appearance === 'default' ? 'info' : appearance;
 
   const baseProps = extractBaseProps(props);
