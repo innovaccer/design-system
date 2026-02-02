60ff50fc57932ff206326531b948af51c701bfae
anuradha9712 <anuradha.mcs18.du@gmail.com>
2026-02-02 16:35:39 +0400

    fix(divider): add role separator and aria-orientation

--- core/components/atoms/divider/Divider.tsx
+++ core/components/atoms/divider/Divider.tsx
@@ -33,7 +33,15 @@ export const Divider = (props: DividerProps) => {
     className
   );
 
-  return <hr data-test="DesignSystem-Divider" {...baseProps} className={DividerClass} />;
+  return (
+    <hr
+      data-test="DesignSystem-Divider"
+      {...baseProps}
+      className={DividerClass}
+      role="separator"
+      aria-orientation={vertical ? 'vertical' : 'horizontal'}
+    />
+  );
 };
 
 Divider.displayName = 'Divider';
